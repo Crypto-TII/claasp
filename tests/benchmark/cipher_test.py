b9c3f6ccfc0154d99a457287215c2e6d8a8f922d
Maria Guerra <maria.guerra@leanmind.es>
2023-05-18 12:56:25 +0100

    Move test files into unit folder. Create benchmark folder and cipher performance tests.

---
+++ tests/benchmark/cipher_test.py
@@ -0,0 +1,59 @@
+import pytest
+import numpy as np
+
+from claasp.ciphers.block_ciphers.aes_block_cipher import AESBlockCipher
+from claasp.ciphers.block_ciphers.speck_block_cipher import SpeckBlockCipher
+
+speck = SpeckBlockCipher()
+aes = AESBlockCipher()
+
+@pytest.mark.parametrize("number_of_samples", [10, 100, 1000, 10000])
+def test_diffusion_tests_with_speck_cipher(benchmark, number_of_samples):
+
+    benchmark(speck.diffusion_tests, number_of_samples=number_of_samples)
+
+
+@pytest.mark.parametrize("number_of_samples", [10, 100, 1000, 10000])
+def test_diffusion_tests_with_aes_cipher(benchmark, number_of_samples):
+
+    benchmark(aes.diffusion_tests, number_of_samples=number_of_samples)
+
+
+def test_evaluate_with_speck_cipher(benchmark):
+    benchmark(speck.evaluate, [0x01234567, 0x89ABCDEF])
+
+
+def test_evaluate_with_aes_cipher(benchmark):
+    benchmark(aes.evaluate, [0x01234567, 0x89ABCDEF])
+
+
+def test_evaluate_using_c_with_speck_cipher(benchmark):
+    benchmark(speck.evaluate_using_c, [0x012345, 0x89ABCD], True)
+
+
+def test_evaluate_using_c_with_aes_cipher(benchmark):
+    benchmark(aes.evaluate_using_c, [0x012345, 0x89ABCD], True)
+
+
+def test_evaluate_vectorized_with_speck_cipher(benchmark):
+    benchmark(speck.evaluate_vectorized, [1, 100, 10000, 1000000])
+
+
+def test_evaluate_vectorized_with_aes_cipher(benchmark):
+    benchmark(aes.evaluate_vectorized, [1, 100, 10000, 1000000])
+
+
+@pytest.mark.parametrize("number_of_samples", [10, 100, 1000, 10000])
+@pytest.mark.parametrize("hidden_layers", [[32, 32, 32], [64, 64, 64]])
+@pytest.mark.parametrize("number_of_epochs", [1, 10, 100])
+def test_neural_network_blackbox_distinguisher_tests_with_speck_cipher(benchmark, number_of_samples,
+                                                                       hidden_layers, number_of_epochs):
+    benchmark(speck.neural_network_blackbox_distinguisher_tests, number_of_samples, hidden_layers, number_of_epochs)
+
+
+@pytest.mark.parametrize("number_of_samples", [10, 100, 1000, 10000])
+@pytest.mark.parametrize("hidden_layers", [[32, 32, 32], [64, 64, 64]])
+@pytest.mark.parametrize("number_of_epochs", [1, 10, 100])
+def test_neural_network_blackbox_distinguisher_tests_with_aes_cipher(benchmark, number_of_samples,
+                                                                     hidden_layers, number_of_epochs):
+    benchmark(aes.neural_network_blackbox_distinguisher_tests, number_of_samples, hidden_layers, number_of_epochs)
