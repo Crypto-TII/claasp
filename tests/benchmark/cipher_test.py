6cad6ea7458afdfa69240f593ac33dbfb753965e
Maria Guerra <maria.guerra@leanmind.es>
2023-05-22 14:03:27 +0100

    Fix benchmark tests.

--- tests/benchmark/cipher_test.py
+++ tests/benchmark/cipher_test.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 
 from claasp.ciphers.block_ciphers.aes_block_cipher import AESBlockCipher
 from claasp.ciphers.block_ciphers.speck_block_cipher import SpeckBlockCipher
@@ -33,19 +34,27 @@ def test_evaluate_using_c_with_aes_cipher(benchmark):
     benchmark(aes.evaluate_using_c, [0x012345, 0x89ABCD], True)
 
 
-def test_evaluate_vectorized_with_speck_cipher(benchmark):
-    benchmark(speck.evaluate_vectorized, [1, 100, 10000, 1000000])
+cipher_inputs_parameter_values = [[np.random.randint(256, size=(8, 2), dtype=np.uint8) for _ in range(10)],
+                                  [np.random.randint(256, size=(8, 2), dtype=np.uint8) for _ in range(100)],
+                                  [np.random.randint(256, size=(8, 2), dtype=np.uint8) for _ in range(10000)],
+                                  [np.random.randint(256, size=(8, 2), dtype=np.uint8) for _ in range(1000000)]]
 
 
-def test_evaluate_vectorized_with_aes_cipher(benchmark):
-    benchmark(aes.evaluate_vectorized, [1, 100, 10000, 1000000])
+@pytest.mark.parametrize("cipher_input", cipher_inputs_parameter_values)
+def test_evaluate_vectorized_with_speck_cipher(benchmark, cipher_input):
+    benchmark(speck.evaluate_vectorized, cipher_input)
+
+
+@pytest.mark.parametrize("cipher_input", cipher_inputs_parameter_values)
+def test_evaluate_vectorized_with_aes_cipher(benchmark, cipher_input):
+    benchmark(aes.evaluate_vectorized, cipher_input)
 
 
 # @pytest.mark.parametrize("number_of_samples", [10, 100, 1000, 10000])
 # @pytest.mark.parametrize("hidden_layers", [[32, 32, 32], [64, 64, 64]])
 # @pytest.mark.parametrize("number_of_epochs", [1, 10, 100])
 def test_neural_network_blackbox_distinguisher_tests_with_speck_cipher(benchmark):
-    benchmark(speck.neural_network_blackbox_distinguisher_tests, number_of_samples=10, hidden_layers=[32, 32, 32],
+    benchmark(speck.neural_network_blackbox_distinguisher_tests, nb_samples=10, hidden_layers=[32, 32, 32],
               number_of_epochs=[1, 10, 100])
 
 
@@ -53,5 +62,5 @@ def test_neural_network_blackbox_distinguisher_tests_with_speck_cipher(benchmark
 # @pytest.mark.parametrize("hidden_layers", [[32, 32, 32], [64, 64, 64]])
 # @pytest.mark.parametrize("number_of_epochs", [1, 10, 100])
 def test_neural_network_blackbox_distinguisher_tests_with_aes_cipher(benchmark):
-    benchmark(aes.neural_network_blackbox_distinguisher_tests, number_of_samples=10, hidden_layers=[32, 32, 32],
+    benchmark(aes.neural_network_blackbox_distinguisher_tests, nb_samples=10, hidden_layers=[32, 32, 32],
               number_of_epochs=[1, 10, 100])
--- tests/benchmark/statistical_tests_test.py
+++ tests/benchmark/statistical_tests_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from claasp.ciphers.block_ciphers.aes_block_cipher import AESBlockCipher
 from claasp.ciphers.block_ciphers.speck_block_cipher import SpeckBlockCipher
 from claasp.cipher_modules.statistical_tests.nist_statistical_tests import StatisticalTests
@@ -11,6 +13,7 @@ def test_run_avalanche_nist_statistics_test_with_speck_cipher(benchmark):
     benchmark(tests.run_avalanche_nist_statistics_test, 0, 10, 10)
 
 
+@pytest.mark.skip("Takes to long")
 def test_run_avalanche_nist_statistics_test_with_aes_cipher(benchmark):
     tests = StatisticalTests(aes)
     benchmark(tests.run_avalanche_nist_statistics_test, 0, 10, 10)
