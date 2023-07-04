5798d28d69f411e0d16d3c73dbb943891cda1414
Emanuele Bellini <eemanuele.bellini@gmail.com>
2023-07-04 10:55:08 +0000

    finalized sbox modelling with ddt probabilities

--- _polito_cryptanalysis_course/automated_cryptanalysis/sbox_model_with_probabilities.py
+++ _polito_cryptanalysis_course/automated_cryptanalysis/sbox_model_with_probabilities.py
@@ -9,7 +9,7 @@ def to_bits(n, x, big_endian=False):
 
 big_endian=False
 
-s = SBox([0, 5, 3, 2, 6, 1, 4, 7]) # Xoodoo and ToySPN sbox, entries are 0, 2, 8
+# s = SBox([0, 5, 3, 2, 6, 1, 4, 7]) # Xoodoo and ToySPN sbox, entries are 0, 2, 8
 s = SBox([5, 3, 4, 6, 2, 7, 0, 1]) # Sbox with entries are 0, 2, 4, 8
 
 print(f'{s = }:')
@@ -20,7 +20,17 @@ n, m = s.input_size(), s.output_size()
 
 # List of non-zero unique entries in the ddt
 nonzero_entries_in_matrix = list(set(ddt.coefficients())) 
+nonzero_entries_in_matrix.remove(2**n)
 
+# Build
+pb_ddts = {}
+for p in nonzero_entries_in_matrix:
+    pb_ddts[p] = matrix(2**n,2**m)
+    for i in range(ddt.nrows()):
+        for j in range(ddt.ncols()):
+            if ddt[i][j] == p:
+               pb_ddts[p][i,j] = 1
+    print(f'{p}-DDT:\n{pb_ddts[p]}')
 
 M = 2*n
 milp = MixedIntegerLinearProgram(maximization=False, solver="GLPK")
@@ -30,35 +40,36 @@ milp = MixedIntegerLinearProgram(maximization=False, solver="GLPK")
 # x8 for the Q variable indicating that the sbox is active
 x = milp.new_variable(integer=True, nonnegative=True)
 
-for i in range(ddt.nrows()):
-    for j in range(ddt.ncols()):
-        if ddt[i][j] != 0:
-            v = to_bits(n, i) + to_bits(n, j)
-            if ddt[i][j] == 2: 
-                ineq = sum(v[h] - (2*v[h]-1)*x[h] for h in range(2*n)) + M*(1-x[6])
+for p in nonzero_entries_in_matrix:
+    print(f'Constraints for {p}-DDT')
+    for i in range(pb_ddts[p].nrows()):
+        for j in range(pb_ddts[p].ncols()):
+            if pb_ddts[p][i][j] == 0:
+                v = to_bits(n, i) + to_bits(n, j)
+                if p == 2:
+                    ineq = sum(v[h] - (2*v[h]-1)*x[h] for h in range(2*n)) + M*(1-x[6])
+                if p == 4:
+                    ineq = sum(v[h] - (2*v[h]-1)*x[h] for h in range(2*n)) + M*(1-x[7])
                 print(f'{v = } : {ineq = }')
                 milp.add_constraint(ineq >= 1)
-            if ddt[i][j] == 4:
-                ineq = sum(v[h] - (2*v[h]-1)*x[h] for h in range(2*n)) + M*(1-x[7])
-                print(f'{v = } : {ineq = }')
-                milp.add_constraint(ineq >= 1)
-
-for i in range(9):
+                
+for i in range(6):
     milp.add_constraint(x[i] <= 1)
 
-
 milp.add_constraint(x[6] + x[7] - x[8] == 0)
-milp.add_constraint(sum(x[i] for i in range(6)) >= 1)
+milp.add_constraint(sum(x[i] for i in range(0,6)) >= 1)
+# milp.add_constraint(sum(x[i] for i in range(3,6)) >= 1)
+milp.add_constraint(x[8] == 1)
 milp.set_objective(2*x[6] + 1*x[7])
 milp.show()
 
-print(f'Minimum value of the bjective function {milp.solve()}')
+print(f'Minimum value of the objective function {milp.solve()}')
 solution = milp.get_values(x)
 print(f'One solution: {solution}')
 
-"""
+
 # Now, let's remove one solution at a time and find the next one
-for i in range(28):
+for i in range(22):
     milp.add_constraint(sum(-(2*solution[i]-1)*x[i] for i in range(6)) + sum(solution[i] for i in range(6))-1 >= 0)
     try:
         milp.solve()
@@ -66,4 +77,4 @@ for i in range(28):
         print(f'Solution {i}: {solution}')
     except:
         print("There is no valid solution")
-"""
+print(ddt)
