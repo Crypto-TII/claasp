3573cd07ea0cb208d12c4958946798faed4b3009
Sharwan Tiwari <sharwantiwari@TII-MAC-STIWARI.local>
2023-09-26 10:27:45 +0400

    added bivium stream cipher and test for trivium stream cipher

--- claasp/ciphers/stream_ciphers/bivium_stream_cipher.py
+++ claasp/ciphers/stream_ciphers/bivium_stream_cipher.py
@@ -19,47 +19,45 @@
 from claasp.cipher import Cipher
 from claasp.name_mappings import INPUT_KEY, INPUT_INITIALIZATION_VECTOR
 
-PARAMETERS_CONFIGURATION_LIST = [{'iv_bit_size': 80, 'key_bit_size': 80, 'state_bit_size': 288,
-                                  'number_of_initialization_clocks': 1152, 'keystream_bit_len': 2 ** 9}]
+PARAMETERS_CONFIGURATION_LIST = [{'iv_bit_size': 80, 'key_bit_size': 80, 'state_bit_size': 177,
+                                  'number_of_initialization_clocks': 4*177, 'keystream_bit_len': 256}]
 
 FSR_DESCR = [
     [
-        [93, [[177], [24], [222], [178, 179]]],  # Register_1: len=93, feedback poly = s_177+ s_24 + s_222 + s_178*s_179
+        [93, [[93], [108], [24], [94, 95]]],  # Register_1: len=93, feedback poly = s_93+ s_108 + s_24 + s_94*s_95
         [84, [[0], [99], [27], [1, 2]]],  # Register_2: len=84, feedback poly = s_0+ s_99 + s_27 + s_1*s_2
-        [111, [[93], [108], [201], [94, 95]]]  # Register_3: len=111, feedback poly = s_93+ s_108 + s_201 + s_94*s_95
     ],
     1  # Registers' cell size = 1-bit
 ]
 
 
-class TriviumStreamCipher(Cipher):
+class BiviumStreamCipher(Cipher):
     """
-    Return a cipher object of Trivium Stream Cipher.
+         Return a cipher object of Bivium Stream Cipher.
 
-    INPUT:
-        - ``keystream_bit_len`` -- **integer** (default: `512`); number of clocks of the cipher.
+        INPUT:
+        - ``keystream_bit_len`` -- **integer** (default: `256`); number of clocks of the cipher.
         - ``key_bit_size`` --  fix 80-bit;
         - ``iv_bit_size`` --   fix 80-bit;
+        EXAMPLES::
 
-    EXAMPLES::
+        sage: from claasp.ciphers.stream_ciphers.bivium_stream_cipher import BiviumStreamCipher
+        sage: biv = BiviumStreamCipher(keystream_bit_len = 2**6)
+        sage: key = 0xffffffffffffffffffff
+        sage: iv = 0xffffffffffffffffffff
+        sage: ks = 0x30d0e5ede563dee6
+        sage: biv.evaluate([key, iv]) == ks
 
-        sage: from claasp.ciphers.stream_ciphers.trivium_stream_cipher import TriviumStreamCipher
-        sage: triv = TriviumStreamCipher(keystream_bit_len=2**8)
-        sage: key = 0x00000000000000000000
-        sage: iv = 0x00000000000000000000
-        sage: ks=0xdf07fd641a9aa0d88a5e7472c4f993fe6a4cc06898e0f3b4e7159ef0854d97b3
-        sage: triv.evaluate([key, iv]) == ks
-        True
     """
 
-    def __init__(self, iv_bit_size=80, key_bit_size=80, state_bit_size=288, number_of_initialization_clocks=1152,
-                 keystream_bit_len=1024):
+    def __init__(self, iv_bit_size=80, key_bit_size=80, state_bit_size=177, number_of_initialization_clocks=708,
+                 keystream_bit_len=2**8):
         self.state_bit_size = state_bit_size
         self.key_bit_size = key_bit_size
         self.iv_bit_size = iv_bit_size
         self.number_of_initialization_clocks = number_of_initialization_clocks
 
-        super().__init__(family_name="trivium_stream_cipher",
+        super().__init__(family_name="bivium_stream_cipher",
                          cipher_type="stream_cipher",
                          cipher_inputs=[INPUT_KEY, INPUT_INITIALIZATION_VECTOR],
                          cipher_inputs_bit_size=[key_bit_size, iv_bit_size],
@@ -67,15 +65,14 @@ class TriviumStreamCipher(Cipher):
 
         iv = [INPUT_INITIALIZATION_VECTOR], [list(range(self.iv_bit_size))]
         key = [INPUT_KEY], [list(range(self.key_bit_size))]
-        key_stream = []
-        self.add_round()
-        triv_state = self.trivium_state_initialization(key, iv)
 
+        biv_state = self.bivium_state_initialization(key, iv)
+        key_stream = []
         for clock_number in range(self._get_len_of_keystream(keystream_bit_len)):
             self.add_round()
-            key_stream = self.trivium_key_stream(triv_state, clock_number, key_stream)
-            triv_state = self.add_FSR_component([triv_state], [list(range(self.state_bit_size))], state_bit_size,
-                                                FSR_DESCR).id
+            key_stream = self.bivium_key_stream(biv_state, clock_number, key_stream)
+            biv_state = self.add_FSR_component([biv_state], [list(range(self.state_bit_size))],
+                                               self.state_bit_size, FSR_DESCR).id
 
         self.add_cipher_output_component([key_stream], [list(range(self._get_len_of_keystream(keystream_bit_len)))],
                                          self._get_len_of_keystream(keystream_bit_len))
@@ -85,36 +82,34 @@ class TriviumStreamCipher(Cipher):
             return keystream_bit_len
         configuration_keystream_bit_len = None
         for parameters in PARAMETERS_CONFIGURATION_LIST:
-            if parameters['iv_bit_size'] == self.iv_bit_size and parameters['key_bit_len'] == self.key_bit_size \
+            if parameters['iv_bit_size'] == self.iv_bit_size and parameters['key_bit_size'] == self.key_bit_size \
                     and parameters['state_bit_size'] == self.state_bit_size:
                 configuration_keystream_bit_len = parameters['keystream_bit_len']
                 break
         if configuration_keystream_bit_len is None:
-            raise ValueError("No available number of clocks for the given parameters.")
+            raise ValueError("No available number of clock for the given parameters.")
         return configuration_keystream_bit_len
 
-    def trivium_state_initialization(self, key, iv):
+    def bivium_state_initialization(self, key, iv):
+        self.add_round()
         cst0 = self.add_constant_component(13, 0x00000).id
         cst1 = self.add_constant_component(4, 0x0).id
-        cst2 = self.add_constant_component(111, 0xe000000000000000000000000000).id
-
-        state0_id = [cst0] + key[0] + [cst1] + iv[0] + [cst2]
-        state0_pos = [list(range(13)), list(range(self.key_bit_size)), list(range(4)), list(range(self.iv_bit_size)),
-                      list(range(111))]
-        triv_state = self.add_FSR_component(state0_id, state0_pos, self.state_bit_size, FSR_DESCR).id
+        state0_id = [cst0] + key[0] + [cst1] + iv[0]
+        state0_pos = [list(range(13)), list(range(self.key_bit_size)), list(range(4)), list(range(self.iv_bit_size))]
+        biv_state = self.add_FSR_component(state0_id, state0_pos, self.state_bit_size, FSR_DESCR).id
 
         for _ in range(1, self.number_of_initialization_clocks):
-            triv_state = self.add_FSR_component([triv_state], [list(range(self.state_bit_size))], self.state_bit_size,
-                                                FSR_DESCR).id
-        return triv_state
+            biv_state = self.add_FSR_component([biv_state], [list(range(self.state_bit_size))],
+                                               self.state_bit_size, FSR_DESCR).id
 
-    def trivium_key_stream(self, state, clock_number, key_st):
+        return biv_state
 
-        key_bit = self.add_XOR_component([state, state, state, state, state, state],
-                                         [[0], [27], [93], [108], [177], [222]], 1).id
+    def bivium_key_stream(self, state, clock_number, key_stream):
+
+        key_bit = self.add_XOR_component([state, state, state, state], [[0], [27], [93], [108]], 1).id
         if clock_number is 0:
             key_stream = self.add_round_output_component([key_bit], [list(range(1))], 1).id
         else:
-            key_stream = self.add_round_output_component([key_st, key_bit], [list(range(clock_number)), [0]],
+            key_stream = self.add_round_output_component([key_stream, key_bit], [list(range(clock_number)), [0]],
                                                          clock_number + 1).id
         return key_stream
