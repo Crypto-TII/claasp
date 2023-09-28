3b1c28f0d46f8d2dd85996f5911be32910f2f3db
MFormenti <math.formenti.97@gmail.com>
2023-09-28 14:49:50 +0400

    Added rc5 block cipher creator

--- claasp/ciphers/block_ciphers/rc5_block_cipher.py
+++ claasp/ciphers/block_ciphers/rc5_block_cipher.py
@@ -1,13 +1,14 @@
 from claasp.cipher import Cipher
 from claasp.name_mappings import INPUT_PLAINTEXT, INPUT_KEY
-from math import ceil
+from math import ceil, floor, log2
 from sage.symbolic.constants import e, golden_ratio
 
-PARAMETERS_CONFIGURATION_LIST = [{'word_size': 32, 'key_size': 128, 'number_of_rounds': 20}]
+PARAMETERS_CONFIGURATION_LIST = [{'word_size': 16, 'key_size': 64, 'number_of_rounds': 16},
+                                 {'word_size': 32, 'key_size': 128, 'number_of_rounds': 20},
+                                 {'word_size': 64, 'key_size': 192, 'number_of_rounds': 24}]
 
 
 def compute_magic_constants(word_size):
-
     Pw = int((e - 2) * (2 ** word_size))
     Qw = int((golden_ratio - 1) * (2 ** word_size))
 
@@ -20,14 +21,202 @@ def compute_magic_constants(word_size):
     return Pw, Qw
 
 
+def key_expansion(self, word_size):
+    Pw, Qw = compute_magic_constants(word_size)
+
+    # we use a dummy component to generate constant components representing the split parts of the key after the first step of the key expansion
+
+    dummy_component = self.add_constant_component(word_size, 0x0)
+
+    # split bit positions orders to adapt to little endian standard
+
+    little_endian_order = [[i for i in range(self.KEY_BLOCK_SIZE)][x:x + 8] for x in
+                           range(0, self.KEY_BLOCK_SIZE, 8)][::-1]
+
+    L = []
+
+    # Key Expansion
+
+    # Step 1 - store the key bytes in an array in groups of 4, using little endian order
+
+    if self.c == 1:
+        L.append(dummy_component)
+    else:
+        for i in range(self.c):
+            if i == self.c - 1:
+                block = (i + 1) * self.u - len(little_endian_order)
+            else:
+                block = self.u
+
+            L.append(self.add_XOR_component(
+                [dummy_component.id] + [INPUT_KEY for _ in
+                                        range(min(self.u, len(little_endian_order) - i * self.u))],
+                [[i for i in range(block * 8)]] + [little_endian_order[-j - 1] for j in
+                                                   range(i * self.u,
+                                                         min((i + 1) * self.u, len(little_endian_order)))][
+                                                  ::-1],
+                word_size))
+
+    # Step 2 - initialize the S vector
+
+    S = [self.add_constant_component(word_size, Pw)]
+    S_value = [Pw]
+
+    for i in range(1, self.t):
+        S_value.append((S_value[i - 1] + Qw) % (2 ** word_size))
+
+        S.append(self.add_constant_component(word_size, S_value[i]))
+
+    # Step 3 - Mix in the secret key
+
+    i = 0
+    j = 0
+    A = dummy_component
+    B = dummy_component
+
+    for n in range(3 * max(self.t, self.c)):
+        # A = (S[i] + A + B) shift 3
+
+        Si_modadd_A = self.add_MODADD_component([S[i].id, A.id],
+                                                [[i for i in range(word_size)] for _ in range(2)],
+                                                word_size)
+
+        Si_modadd_A_modadd_B = self.add_MODADD_component([Si_modadd_A.id, B.id],
+                                                         [[i for i in range(word_size)] for _ in range(2)],
+                                                         word_size)
+
+        A = self.add_rotate_component([Si_modadd_A_modadd_B.id],
+                                      [[i for i in range(word_size)]],
+                                      word_size,
+                                      -3)
+
+        S[i] = A
+
+        # B = (key_array[j] + A + B) shift (A + B)
+
+        A_modadd_B = self.add_MODADD_component([A.id, B.id],
+                                               [[i for i in range(word_size)] for _ in range(2)],
+                                               word_size)
+
+        shift_amount = self.add_XOR_component([A_modadd_B.id, dummy_component.id],
+                                              [[word_size - 1 - i for i in range(self.Lgw)][::-1],
+                                               [i for i in range(word_size)]],
+                                              self.Lgw)
+
+        Lj_modadd_A = self.add_MODADD_component([L[j].id, A.id],
+                                                [[i for i in range(word_size)] for _ in range(2)],
+                                                word_size)
+
+        Lj_modadd_A_modadd_B = self.add_MODADD_component([Lj_modadd_A.id, B.id],
+                                                         [[i for i in range(word_size)] for _ in range(2)],
+                                                         word_size)
+
+        B = self.add_variable_rotate_component([Lj_modadd_A_modadd_B.id, shift_amount.id],
+                                               [[i for i in range(word_size)], [i for i in range(self.Lgw)]],
+                                               word_size,
+                                               -1)
+
+        L[j] = B
+
+        i = (i + 1) % self.t
+        j = (j + 1) % self.c
+
+    return S, L
+
+
+def first_round(self, S, word_size):
+    # Round 1
+
+    dummy_component = self.add_constant_component(word_size, 0x0)
+
+    little_endian_order_pt = [[i for i in range(2 * word_size)][x:x + 8] for x in range(0, 2 * word_size, 8)][::-1]
+
+    A = self.add_XOR_component([INPUT_PLAINTEXT for _ in range(int(word_size / 8))] + [dummy_component.id],
+                               [little_endian_order_pt[j] for j in
+                                range(int(word_size / 8), int(word_size / 4))] + [[i for i in range(word_size)]],
+                               word_size)
+
+    B = self.add_XOR_component([INPUT_PLAINTEXT for _ in range(int(word_size / 8))] + [dummy_component.id],
+                               [little_endian_order_pt[j] for j in range(int(word_size / 8))] + [
+                                   [i for i in range(word_size)]],
+                               word_size)
+
+    S0_modadd = self.add_MODADD_component([A.id, S[0].id],
+                                          [[i for i in range(word_size)] for _ in range(2)],
+                                          word_size)
+
+    S1_modadd = self.add_MODADD_component([B.id, S[1].id],
+                                          [[i for i in range(word_size)] for _ in range(2)],
+                                          word_size)
+
+    A = S0_modadd
+    B = S1_modadd
+
+    return A, B
+
+
+def round_function(self, k, A, B, S, word_size):
+    dummy_component = self.add_constant_component(word_size, 0x0)
+
+    A_xor_B = self.add_XOR_component([A.id, B.id],
+                                     [[i for i in range(word_size)] for _ in range(2)],
+                                     word_size)
+
+    shift_amount_B = self.add_XOR_component([B.id, dummy_component.id],
+                                            [[word_size - 1 - i for i in range(self.Lgw)][::-1],
+                                             [i for i in range(word_size)]],
+                                            self.Lgw)
+
+    B_shift = self.add_variable_rotate_component([A_xor_B.id, shift_amount_B.id],
+                                                 [[i for i in range(word_size)], [i for i in range(self.Lgw)]],
+                                                 word_size,
+                                                 -1)
+
+    S_2i_modadd = self.add_MODADD_component([B_shift.id, S[2 * (k + 1)].id],
+                                            [[i for i in range(word_size)] for _ in range(2)],
+                                            word_size)
+
+    A = S_2i_modadd
+
+    B_xor_A = self.add_XOR_component([B.id, A.id],
+                                     [[i for i in range(word_size)] for _ in range(2)],
+                                     word_size)
+
+    shift_amount_A = self.add_XOR_component([A.id, dummy_component.id],
+                                            [[word_size - 1 - i for i in range(self.Lgw)][::-1],
+                                             [i for i in range(word_size)]],
+                                            self.Lgw)
+
+    A_shift = self.add_variable_rotate_component([B_xor_A.id, shift_amount_A.id],
+                                                 [[i for i in range(word_size)], [i for i in range(self.Lgw)]],
+                                                 word_size,
+                                                 -1)
+
+    S_2i_1_modadd = self.add_MODADD_component([A_shift.id, S[2 * (k + 1) + 1].id],
+                                              [[i for i in range(word_size)] for _ in range(2)],
+                                              word_size)
+
+    B = S_2i_1_modadd
+
+    if k != self.NROUNDS - 1:
+        self.add_round()
+
+    return A, B
+
+
 class RC5BlockCipher(Cipher):
 
     def __init__(self, number_of_rounds=12, word_size=32, key_size=128):
 
         self.CIPHER_BLOCK_SIZE = 2 * word_size
-        self.KEY_BLOCK_SIZE = key_size
+
+        if key_size == 0:
+            self.KEY_BLOCK_SIZE = 1
+        else:
+            self.KEY_BLOCK_SIZE = key_size
         self.NROUNDS = number_of_rounds
 
+        self.Lgw = int(floor(log2(word_size)))
         self.u = int(word_size / 8)
         self.t = 2 * (number_of_rounds + 1)
         self.c = int(ceil(self.KEY_BLOCK_SIZE / (8 * self.u)))
@@ -41,185 +230,25 @@ class RC5BlockCipher(Cipher):
                          cipher_inputs_bit_size=[self.KEY_BLOCK_SIZE, self.CIPHER_BLOCK_SIZE],
                          cipher_output_bit_size=self.CIPHER_BLOCK_SIZE)
 
-        # Magic constants are defined by the choice of word size. since the word size can be any value from 0 to 2040 bits,
-        # Pw = Odd((e-2) * 2^w)
-        # Qw = Pdd((phi - 1) * 2^w)
-        #
-        # Odd(x) is the closest odd integer to x, rounded up. phi is the golden ratio, w is the word_size in bytes.
-
-        Pw, Qw = compute_magic_constants(word_size)
-
-        # First magic constant, in function of word size
-
-        # Key Initialization
-
-        self.add_round()
-
-        # we use a dummy component to generate constant components representing the split parts of the key after the first step of the key expansion
-
-        dummy_component = self.add_constant_component(word_size, 0x0)
-
-        # split bit positions orders to adapt to little endian standard
-
-        little_endian_order = [[i for i in range(self.KEY_BLOCK_SIZE)][x:x + 8] for x in
-                               range(0, self.KEY_BLOCK_SIZE, 8)][::-1]
-
-        L = []
-
         # Key Expansion
 
-        # Step 1 - store the key bytes in an array in groups of 4, using little endian order
-
-        i = 0
-
-
-        if self.c == 1:
-            L.append(dummy_component)
-        else:
-            for i in range(self.c):
-                    if i == self.c - 1:
-                        block = (i+1) * self.u - len(little_endian_order)
-                    else :
-                        block = self.u
-
-                    L.append(self.add_XOR_component(
-                        [dummy_component.id] + [INPUT_KEY for _ in range(min(self.u, len(little_endian_order) - i * self.u ))],
-                        [[i for i in range(block * 8 )]] + [little_endian_order[-j-1] for j in range(i * self.u, min((i + 1) * self.u, len(little_endian_order)))][::-1],
-                        word_size))
-
-
-
-        # Step 2 - initialize the S vector
-
-        S = [self.add_constant_component(word_size, Pw)]
-        S_value = [Pw]
-        for i in range(1, self.t):
-            S_value.append((S_value[i - 1] + Qw) % (2 ** word_size))
-
-            S.append(self.add_constant_component(word_size, S_value[i]))
-
-
-        # Step 3 - Mix in the secret key
-
-        i = 0
-        j = 0
-        A = dummy_component
-        B = dummy_component
-
-
-
-
-        for n in range(3 * max(self.t, self.c)):
-            # A = (S[i] + A + B) shift 3
-
-
-            A_modadd_B = self.add_MODADD_component([A.id, B.id],
-                                                   [[i for i in range(word_size)] for _ in range(2)],
-                                                   word_size)
-
-            Si_modadd_A_modadd_B = self.add_MODADD_component([S[i].id, A_modadd_B.id],
-                                                             [[i for i in range(word_size)] for _ in range(2)],
-                                                             word_size)
-
-            S[i] = self.add_rotate_component([Si_modadd_A_modadd_B.id],
-                                             [[i for i in range(word_size)]],
-                                             word_size,
-                                             -3)
-
-            A = S[i]
-
-
-            # B = (key_array[j] + A + B) shift (A + B)
-
-
-
-
-            B_modadd_A = self.add_MODADD_component([B.id, A.id],
-                                                   [[i for i in range(word_size)] for _ in range(2)],
-                                                   word_size)
-
-            Lj_modadd_A_modadd_B = self.add_MODADD_component([L[j].id, B_modadd_A.id],
-                                                             [[i for i in range(word_size)] for _ in range(2)],
-                                                             word_size)
-
-
-            L[j] = self.add_variable_rotate_component([Lj_modadd_A_modadd_B.id, B_modadd_A.id],
-                                                      [[i for i in range(word_size)] for _ in range(2)],
-                                                      word_size,
-                                                      -1)
-
-            B = L[j]
-
-            i = (i + 1) % self.t
-            j = (j + 1) % self.c
+        self.add_round()
+        S, L = key_expansion(self, word_size)
 
         # Encryption
 
-        # Round 1
+        A, B = first_round(self, S, word_size)
 
-        A = dummy_component
-        B = dummy_component
+        for k in range(number_of_rounds):
+            A, B = round_function(self, k, A, B, S, word_size)
 
-        little_endian_order_pt = [[i for i in range(2 * word_size)][x:x + 8] for x in range(0, 2 * word_size, 8)][::-1]
+        # Output
 
-        A = self.add_XOR_component([INPUT_PLAINTEXT for _ in range(int(word_size / 8))] + [dummy_component.id],
-                                   [little_endian_order_pt[j] for j in range(int(word_size / 8))] + [
-                                       [i for i in range(word_size)]],
-                                   word_size)
-
-        B = self.add_XOR_component([INPUT_PLAINTEXT for _ in range(int(word_size / 8))] + [dummy_component.id],
-                                   [little_endian_order_pt[j] for j in
-                                    range(int(word_size / 8), int(word_size / 4))] + [[i for i in range(word_size)]],
-                                   word_size)
-
-        S0_modadd = self.add_MODADD_component([A.id, S[0].id],
-                                              [[i for i in range(word_size)] for _ in range(2)],
-                                              word_size)
-
-        S1_modadd = self.add_MODADD_component([B.id, S[0].id],
-                                              [[i for i in range(word_size)] for _ in range(2)],
-                                              word_size)
-
-        A = S0_modadd
-        B = S1_modadd
-
-
-        # ----
-
-        for i in range(number_of_rounds):
-
-            A_xor_B = self.add_XOR_component([A.id, B.id],
-                                             [[i for i in range(word_size)] for _ in range(2)],
-                                             word_size)
-
-            B_shift = self.add_variable_rotate_component([A_xor_B.id, B.id],
-                                                         [[i for i in range(word_size)] for _ in range(2)],
-                                                         word_size,
-                                                         -1)
-            S_2i_modadd = self.add_MODADD_component([B_shift.id, S[2 * i].id],
-                                                    [[i for i in range(word_size)] for _ in range(2)],
-                                                    word_size)
-
-            A = S_2i_modadd
-
-            A_xor_B = self.add_XOR_component([A.id, B.id],
-                                             [[i for i in range(word_size)] for _ in range(2)],
-                                             word_size)
-
-            A_shift = self.add_variable_rotate_component([A_xor_B.id, A.id],
-                                                         [[i for i in range(word_size)] for _ in range(2)],
-                                                         word_size,
-                                                         -1)
-
-            S_2i_1_modadd = self.add_MODADD_component([A_shift.id, S[2 * i + 1].id],
-                                                      [[i for i in range(word_size)] for _ in range(2)],
-                                                      word_size)
-
-            B = S_2i_1_modadd
-
-            if i != number_of_rounds - 1:
-                self.add_round()
+        big_endian_order_output = [[i for i in range(word_size)][x:x + 8] for x in range(0, word_size, 8)][::-1]
+        output_order = []
+        for o in big_endian_order_output:
+            output_order = output_order + o
 
         self.add_cipher_output_component([A.id, B.id],
-                                         [[i for i in range(word_size)] for _ in range(2)],
+                                         [output_order for _ in range(2)],
                                          2 * word_size)
---
+++ tests/unit/ciphers/block_ciphers/rc5_block_cipher_test.py
@@ -0,0 +1,57 @@
+from claasp.ciphers.block_ciphers.rc5_block_cipher import RC5BlockCipher
+
+
+def test_rc5_block_cipher():
+    rc5 = RC5BlockCipher()
+    assert rc5.type == 'block_cipher'
+    assert rc5.family_name == 'rc5_block_cipher'
+    assert rc5.number_of_rounds == 12
+    assert rc5.id == 'rc5_block_cipher_k128_p64_o64_r12'
+    assert rc5.component_from(0, 0).id == 'constant_0_0'
+
+    rc5 = RC5BlockCipher(number_of_rounds=4)
+    assert rc5.number_of_rounds == 4
+    assert rc5.id == 'rc5_block_cipher_k64_p64_o64_r4'
+    assert rc5.component_from(3, 1).id == 'xor_3_1'
+
+    rc5 = RC5BlockCipher(word_size=8, number_of_rounds=12, key_size=32)
+    key = 0x00010203
+    plaintext = 0x0001
+    ciphertext = 0x212a
+    assert rc5.evaluate([key, plaintext]) == ciphertext
+
+    rc5 = RC5BlockCipher(word_size=16, number_of_rounds=16, key_size=64)
+    key = 0x0001020304050607
+    plaintext = 0x00010203
+    ciphertext = 0x23a8d72e
+    assert rc5.evaluate([key, plaintext]) == ciphertext
+
+    rc5 = RC5BlockCipher(word_size=32, number_of_rounds=20, key_size=128)
+    key = 0x000102030405060708090A0B0C0D0E0F
+    plaintext = 0x0123456789ABCDEF
+    ciphertext = 0x2A0EDC0E9431FF73
+    assert rc5.evaluate([key, plaintext]) == ciphertext
+
+    rc5 = RC5BlockCipher(word_size=64, number_of_rounds=24, key_size=192)
+    key = 0x000102030405060708090A0B0C0D0E0F1011121314151617
+    plaintext = 0x000102030405060708090A0B0C0D0E0F
+    ciphertext = 0xA46772820EDBCE0235ABEA32AE7178DA
+    assert rc5.evaluate([key, plaintext]) == ciphertext
+
+    rc5 = RC5BlockCipher(word_size=128, number_of_rounds=28, key_size=256)
+    key = 0x000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F
+    plaintext = 0x000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F
+    ciphertext = 0xECA5910921A4F4CFDD7AD7AD20A1FCBA068EC7A7CD752D68FE914B7FE180B440
+    assert rc5.evaluate([key, plaintext]) == ciphertext
+
+    rc5 = RC5BlockCipher(word_size=24, number_of_rounds=4, key_size=1)
+    key = 0x0
+    plaintext = 0x000102030405
+    ciphertext = 0x89CBDCC9525A
+    assert rc5.evaluate([key, plaintext]) == ciphertext
+
+    rc5 = RC5BlockCipher(word_size=80, number_of_rounds=4, key_size=96)
+    key = 0x000102030405060708090A0B
+    plaintext = 0x000102030405060708090A0B0C0D0E0F10111213
+    ciphertext = 0x9CB59ECBA4EA84568A4278B0E132D5FC9D5819D6
+    assert rc5.evaluate([key, plaintext]) == ciphertext
