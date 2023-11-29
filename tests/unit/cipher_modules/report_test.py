5d38055af2125848a9b7cea1df385df2736e0983
MFormenti <math.formenti.97@gmail.com>
2023-11-29 13:36:31 +0400

    Updated report_test.py

--- tests/unit/cipher_modules/report_test.py
+++ tests/unit/cipher_modules/report_test.py
@@ -27,10 +27,30 @@ def test_print_report():
 def test_save_as_latex_table():
 
     simon = SimonBlockCipher(number_of_rounds=3)
+    smt = SmtXorDifferentialModel(simon)
+
+    plaintext = set_fixed_variables(
+        component_id='plaintext',
+        constraint_type='not_equal',
+        bit_positions=range(32),
+        bit_values=(0,) * 32)
+    key = set_fixed_variables(
+        component_id='key',
+        constraint_type='equal',
+        bit_positions=range(64),
+        bit_values=(0,) * 64)
+
+    trail = smt.find_lowest_weight_xor_differential_trail(fixed_values=[plaintext, key])
 
     avalanche_test_results = simon.diffusion_tests()
     avalanche_report = Report(simon, avalanche_test_results)
     avalanche_report.save_as_latex_table()
+
+    trail_report = Report(simon, trail)
+
+    trail_report.save_as_latex_table()
+
+
 
 def test_save_as_DataFrame():
 
@@ -48,6 +68,12 @@ def test_save_as_DataFrame():
         bit_values=(0,) * 64)
     trail = smt.find_lowest_weight_xor_differential_trail(fixed_values=[plaintext, key])
 
+    algebraic_results = speck.algebraic_tests(timeout=1)
+
+    algebraic_report = Report(speck, algebraic_results)
+    algebraic_report.save_as_DataFrame()
+
+
     trail_report = Report(speck, trail)
     trail_report.save_as_DataFrame()
 
@@ -57,6 +83,24 @@ def test_save_as_json():
     neural_network_blackbox_distinguisher_tests_results = simon.neural_network_blackbox_distinguisher_tests()
     blackbox_report = Report(simon,neural_network_blackbox_distinguisher_tests_results)
 
+    sat = SatXorDifferentialModel(simon)
+    plaintext = set_fixed_variables(
+        component_id='plaintext',
+        constraint_type='not_equal',
+        bit_positions=range(32),
+        bit_values=(0,) * 32)
+    key = set_fixed_variables(
+        component_id='key',
+        constraint_type='equal',
+        bit_positions=range(64),
+        bit_values=(0,) * 64)
+
+    trail = sat.find_lowest_weight_xor_differential_trail(fixed_values=[plaintext, key])
+
+    trail_report = Report(simon, trail)
+
+
+    trail_report.save_as_json()
     blackbox_report.save_as_json()
 
 def test_clean_reports():
