2cd2049223f8b2342637e23cd657a89750f8a206
MFormenti <math.formenti.97@gmail.com>
2023-12-01 13:27:03 +0400

    rebased branch for merge with develop

--- tests/unit/cipher_modules/report_test.py
+++ tests/unit/cipher_modules/report_test.py
@@ -1,5 +1,7 @@
 from claasp.cipher_modules.models.sat.sat_models.sat_xor_differential_model import SatXorDifferentialModel
 from claasp.cipher_modules.models.smt.smt_models.smt_xor_differential_model import SmtXorDifferentialModel
+from claasp.cipher_modules.models.cp.cp_models.cp_xor_differential_model import CpXorDifferentialModel
+from claasp.cipher_modules.models.milp.milp_models.milp_xor_differential_model import MilpXorDifferentialModel
 from claasp.cipher_modules.models.utils import set_fixed_variables
 from claasp.ciphers.block_ciphers.speck_block_cipher import SpeckBlockCipher
 from claasp.ciphers.block_ciphers.simon_block_cipher import SimonBlockCipher
@@ -9,7 +11,7 @@ from claasp.cipher_modules.statistical_tests.nist_statistical_tests import Stati
 
 def test_print_report():
 
-    speck = SpeckBlockCipher(number_of_rounds=5)
+    speck = SpeckBlockCipher(number_of_rounds=2)
     sat = SatXorDifferentialModel(speck)
     plaintext = set_fixed_variables(
         component_id = 'plaintext',
@@ -37,11 +39,13 @@ def test_print_report():
     nist_report = Report(speck, parsed_result_nist)
     dieharder_report = Report(speck, parsed_result_dieharder)
 
+
+
     nist_report.print_report()
     dieharder_report.print_report()
 def test_save_as_latex_table():
 
-    simon = SimonBlockCipher(number_of_rounds=3)
+    simon = SimonBlockCipher(number_of_rounds=2)
     smt = SmtXorDifferentialModel(simon)
 
     plaintext = set_fixed_variables(
@@ -61,13 +65,14 @@ def test_save_as_latex_table():
     avalanche_report = Report(simon, avalanche_test_results)
     avalanche_report.save_as_latex_table()
 
+
     trail_report = Report(simon, trail)
     trail_report.save_as_latex_table()
 
 
 def test_save_as_DataFrame():
-    speck = SpeckBlockCipher(number_of_rounds=4)
-    smt = SmtXorDifferentialModel(speck)
+    speck = SpeckBlockCipher(number_of_rounds=2)
+    smt = CpXorDifferentialModel(speck)
     plaintext = set_fixed_variables(
         component_id='plaintext',
         constraint_type='not_equal',
@@ -90,11 +95,11 @@ def test_save_as_DataFrame():
 
 def test_save_as_json():
 
-    simon = SimonBlockCipher(number_of_rounds=6)
+    simon = SimonBlockCipher(number_of_rounds=3)
     neural_network_blackbox_distinguisher_tests_results = simon.neural_network_blackbox_distinguisher_tests()
     blackbox_report = Report(simon,neural_network_blackbox_distinguisher_tests_results)
 
-    sat = SatXorDifferentialModel(simon)
+    milp = MilpXorDifferentialModel(simon)
     plaintext = set_fixed_variables(
         component_id='plaintext',
         constraint_type='not_equal',
@@ -106,17 +111,22 @@ def test_save_as_json():
         bit_positions=range(64),
         bit_values=(0,) * 64)
 
-    trail = sat.find_lowest_weight_xor_differential_trail(fixed_values=[plaintext, key])
+    trail = milp.find_lowest_weight_xor_differential_trail(fixed_values=[plaintext, key])
 
     trail_report = Report(simon, trail)
 
 
+    algebraic_results = simon.algebraic_tests(timeout=1)
+    algebraic_report = Report(simon, algebraic_results)
+    algebraic_report.save_as_json()
+
     trail_report.save_as_json()
     blackbox_report.save_as_json()
 
+
 def test_clean_reports():
 
-    simon = SimonBlockCipher(number_of_rounds=3)
+    simon = SimonBlockCipher(number_of_rounds=2)
     neural_network_blackbox_distinguisher_tests_results = simon.neural_network_blackbox_distinguisher_tests()
     blackbox_report = Report(simon, neural_network_blackbox_distinguisher_tests_results)
 
