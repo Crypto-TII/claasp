f9978defc2a2ef8cc032dfdd567ec24ad5a132ad
Sharwan Tiwari <sharwantiwari@TII-MAC-STIWARI.local>
2024-01-31 10:00:02 +0400

    added tests for gaston permutation

--- claasp/ciphers/permutations/gaston_permutation.py
+++ claasp/ciphers/permutations/gaston_permutation.py
@@ -22,6 +22,7 @@ from claasp.DTOs.component_state import ComponentState
 
 GASTON_NROWS = 5
 WORD_SIZE = 64
+
 # parameters for theta
 GASTON_t = [25, 32, 52, 60, 63]
 # GASTON_t0 = 25
@@ -49,6 +50,7 @@ GASTON_w = [0, 56, 31, 46, 43]
 # GASTON_w2 = 31
 # GASTON_w3 = 46
 # GASTON_w4 = 43
+
 # gaston round constant
 gaston_rc = [
     0x00000000000000F0, 0x00000000000000E1, 0x00000000000000D2,
@@ -76,18 +78,21 @@ class GastonPermutation(Cipher):
         sage: ciphertext = 0x88B326096BEBC6356CA8FB64BC5CE6CAF1CE3840D819071354D70067438689B5F17FE863F958F32B
         sage: print(gaston.evaluate([plaintext]))==ciphertext)
         True
+
         sage: plaintext=0x1F4AD9906DA6A2544B84D7F83F2BDDFA468A0853578A00E36C05A0506DF7F66E4EFB22112453C964
         sage: ciphertext=0x1BA89B5B5C4583B622135709AE53417D9847B975E9EC9F3DCE042DF2A402591D563EC68FC30307EA
         sage: print(gaston.evaluate([plaintext])==ciphertext)
         True
+
         sage: plaintext=0xFFFFFFFFFFFFFFFF0123456789ABCDEFFEDCBA9876543210AAAAAAAAAAAAAAAA0101010101010101
         sage: ciphertext=0x3117D51B14937067338F17F773C13F79DFB86E0868D252AB0D461D35EB863DE708BCE3E354C7231A
         sage: print(gaston.evaluate([plaintext])==ciphertext)
         True
+
         sage: gaston.number_of_rounds
         12
         sage: gaston.component_from(0, 0).id
-        'constant_0_0'
+        'rot_0_0'
     """
 
     def __init__(self, number_of_rounds=12):
@@ -99,28 +104,22 @@ class GastonPermutation(Cipher):
                          cipher_inputs_bit_size=[self.state_bit_size],
                          cipher_output_bit_size=self.state_bit_size)
 
-        # word initialization
+        # gaston state initialization
         state = []
-        for i in range(GASTON_NROWS):
-            p = ComponentState([INPUT_PLAINTEXT], [[k + i * WORD_SIZE for k in range(WORD_SIZE)]])
+        for row in range(GASTON_NROWS):
+            p = ComponentState([INPUT_PLAINTEXT], [[i + row * WORD_SIZE for i in range(WORD_SIZE)]])
             state.append(p)
 
-        # round function
-        for r in range(12 - number_of_rounds, 12):
-            # initial current round element
+        for round_number in range(12 - number_of_rounds, 12):
             self.add_round()
-            # round function
-            state = self.gaston_round_function(state, gaston_rc[r])
-            # round output
-            inputs = []
-            for i in range(GASTON_NROWS):
-                inputs.append(state[i])
-            inputs_id, inputs_pos = get_inputs_parameter(inputs)
-            if r == 11:
+            # gaston round function
+            state = self.gaston_round_function(state, gaston_rc[round_number])
+            # gaston round output
+            inputs_id, inputs_pos = get_inputs_parameter([state[i] for i in range(GASTON_NROWS)])
+            if round_number == 11:
                 self.add_cipher_output_component(inputs_id, inputs_pos, self.state_bit_size)
             else:
                 self.add_round_output_component(inputs_id, inputs_pos, self.state_bit_size)
-            # add all components to the round
 
     def gaston_round_function(self, state, rc):
         state = self.gaston_rho_east(state)
@@ -132,9 +131,10 @@ class GastonPermutation(Cipher):
         return state
 
     def gaston_rho_east(self, state):
-        for i in range(GASTON_NROWS):
-            self.add_rotate_component(state[i].id, state[i].input_bit_positions, WORD_SIZE, -GASTON_e[i])
-            state[i] = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
+        for row in range(GASTON_NROWS):
+            self.add_rotate_component(state[row].id, state[row].input_bit_positions, WORD_SIZE, -GASTON_e[row])
+            state[row] = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
+
         return state
 
     def gaston_theta(self, state):
@@ -148,14 +148,15 @@ class GastonPermutation(Cipher):
         inputs_id, inputs_pos = get_inputs_parameter([P, P_rot])
         self.add_XOR_component(inputs_id, inputs_pos, WORD_SIZE)
         P = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
+        # column parity P
 
-        Q_Rows = []
+        Q_rows = []
         for i in range(GASTON_NROWS):
             self.add_rotate_component(state[i].id, state[i].input_bit_positions, WORD_SIZE, -GASTON_t[i])
             q = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
-            Q_Rows.append(q)
+            Q_rows.append(q)
 
-        inputs_id, inputs_pos = get_inputs_parameter([Q_Rows[i] for i in range(GASTON_NROWS)])
+        inputs_id, inputs_pos = get_inputs_parameter([Q_rows[i] for i in range(GASTON_NROWS)])
         self.add_XOR_component(inputs_id, inputs_pos, WORD_SIZE)
         Q = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
 
@@ -165,6 +166,7 @@ class GastonPermutation(Cipher):
         inputs_id, inputs_pos = get_inputs_parameter([Q, Q_rot])
         self.add_XOR_component(inputs_id, inputs_pos, WORD_SIZE)
         Q = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
+        # column parity Q
 
         inputs_id, inputs_pos = get_inputs_parameter([P, Q])
         self.add_XOR_component(inputs_id, inputs_pos, WORD_SIZE)
@@ -173,16 +175,18 @@ class GastonPermutation(Cipher):
         self.add_rotate_component(P.id, P.input_bit_positions, WORD_SIZE, -GASTON_u)
         P = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
 
-        for i in range(GASTON_NROWS):
-            inputs_id, inputs_pos = get_inputs_parameter([state[i], P])
+        for row in range(GASTON_NROWS):
+            inputs_id, inputs_pos = get_inputs_parameter([state[row], P])
             self.add_XOR_component(inputs_id, inputs_pos, WORD_SIZE)
-            state[i] = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
+            state[row] = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
+
         return state
 
     def gaston_rho_west(self, state):
-        for i in range(GASTON_NROWS):
-            self.add_rotate_component(state[i].id, state[i].input_bit_positions, WORD_SIZE, -GASTON_w[i])
-            state[i] = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
+        for row in range(GASTON_NROWS):
+            self.add_rotate_component(state[row].id, state[row].input_bit_positions, WORD_SIZE, -GASTON_w[row])
+            state[row] = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
+
         return state
 
     def gaston_iota(self, state, rc):
@@ -196,21 +200,22 @@ class GastonPermutation(Cipher):
 
     def gaston_chi(self, state):
         not_comp = []
-        for i in range(GASTON_NROWS):
-            self.add_NOT_component(state[i].id, state[i].input_bit_positions, WORD_SIZE)
+        for row in range(GASTON_NROWS):
+            self.add_NOT_component(state[row].id, state[row].input_bit_positions, WORD_SIZE)
             n = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
             not_comp.append(n)
 
         and_comp = []
-        for i in range(GASTON_NROWS):
+        for row in range(GASTON_NROWS):
             inputs_id, inputs_pos = get_inputs_parameter(
-                [state[(i + 2) % GASTON_NROWS], not_comp[(i + 1) % GASTON_NROWS]])
+                [state[(row + 2) % GASTON_NROWS], not_comp[(row + 1) % GASTON_NROWS]])
             self.add_AND_component(inputs_id, inputs_pos, WORD_SIZE)
             a = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
             and_comp.append(a)
 
-        for i in range(GASTON_NROWS):
-            inputs_id, inputs_pos = get_inputs_parameter([state[i], and_comp[i]])
+        for row in range(GASTON_NROWS):
+            inputs_id, inputs_pos = get_inputs_parameter([state[row], and_comp[row]])
             self.add_XOR_component(inputs_id, inputs_pos, WORD_SIZE)
-            state[i] = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
+            state[row] = ComponentState([self.get_current_component_id()], [list(range(WORD_SIZE))])
+
         return state
