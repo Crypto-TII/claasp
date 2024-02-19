5e957cfd18dccc9317ee398b454e93c7650dac51
Sharwan Tiwari <sharwantiwari@TII-MAC-STIWARI.local>
2024-02-19 17:26:51 +0400

    fixed a typo in algebraic tests

--- tests/unit/cipher_test.py
+++ tests/unit/cipher_test.py
@@ -64,7 +64,7 @@ def test_algebraic_tests():
     aes = AESBlockCipher(word_size=4, state_size=2, number_of_rounds=2)
     d = AlgebraicTest(aes).algebraic_tests(5)
     compare_result = {'input_parameters': {'cipher.id': 'aes_block_cipher_k16_p16_o16_r2',
-                                           'timeout': 30,
+                                           'timeout': 5,
                                            'test_name': 'algebraic_tests'},
                       'test_results': {'number_of_variables': [352, 592],
                                        'number_of_equations': [454, 796],
