618b5a7e8a6b52ccbdccc65080b44f200183f775
simopez <simonepelizzola@gmail.com>
2024-04-26 10:14:12 +0200

    minor fix

--- tests/unit/components/or_component_test.py
+++ tests/unit/components/or_component_test.py
@@ -38,8 +38,8 @@ def test_cp_xor_linear_mask_propagation_constraints():
     assert declarations == ['array[0..31] of var 0..3200: p_or_39_6;', 'array[0..63] of var 0..1:or_39_6_i;',
                             'array[0..31] of var 0..1:or_39_6_o;']
 
-    assert constraints[0] == 'constraint table(or_39_6_i[0]++or_39_6_i[32]++or_39_6_o[0]++p_or_39_6[0],and2inputs_LAT);'
-    assert constraints[-2] == 'constraint table(or_39_6_i[31]++or_39_6_i[63]++or_39_6_o[31]++p_or_39_6[31],' \
+    assert constraints[0] == 'constraint table([or_39_6_i[0]]++[or_39_6_i[32]]++[or_39_6_o[0]]++[p_or_39_6[0]],and2inputs_LAT);'
+    assert constraints[-2] == 'constraint table([or_39_6_i[31]]++[or_39_6_i[63]]++[or_39_6_o[31]]++[p_or_39_6[31]],' \
                               'and2inputs_LAT);'
     assert constraints[-1] == 'constraint p[0] = sum(p_or_39_6);'
 
