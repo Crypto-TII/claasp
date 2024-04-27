9c668b1ef30a5ebd80358fafe12a84c49a9de495
Mohamed Rachidi <mohamed.rachidi@tii.ae>
2024-04-27 13:24:51 +0400

    key recovery seems to work when nb_pairs = 10000. Need to increase the search space to be certain

--- claasp/cipher_modules/linear_key_recovery_DES.py
+++ claasp/cipher_modules/linear_key_recovery_DES.py
@@ -12,6 +12,7 @@ def test_vector_vectorized(number_rounds=16):
     from claasp.cipher_modules.linear_key_recovery_DES import *
     test_vector_vectorized()
     """
+    # The following vector test is from "Validating the correctness of hardware implementations of DES".
     # For k = 0x10316e028c8f3b4a and p = 0, we should get c = 0x82dcbafbdeab6602
     start_time = datetime.now()
     cipher = DESBlockCipher(number_of_rounds=number_rounds)
@@ -74,7 +75,7 @@ def xor_bits(state32, position):
     from claasp.cipher_modules.linear_key_recovery_DES import *
     s = 0xabcd0123
     xor_bits(s, [1,2])
-    # position is a list of indexes that starts from 1 to the left (msb)
+    # position is a list of indexes that starts from 1 and from the left (msb)
     """
     res = 0
     for i in position:
@@ -127,6 +128,10 @@ def test_linear_approx_on_multiple_pair_vectorized():
 
 def gen_partial_subkey(i):
     """
+    The bits that need to be guessed for the subkey6 are those in position indicated by the 1s:
+    000000111111000000111111111111111111000000111111
+    This method generate a candidate of subkey6 with respect the above positions.
+
     from claasp.cipher_modules.linear_key_recovery_DES import *
     hex(gen_partial_subkey(44658535))
     """
@@ -145,7 +150,7 @@ def gen_partial_subkey(i):
 def partial_subkey_recovery_vectorized():
     """
     from claasp.cipher_modules.linear_key_recovery_DES import *
-    partial_subkey_recovery_vectorized()
+    true_partial_subkey, Juan = partial_subkey_recovery_vectorized()
     """
     cipher = DESBlockCipher(number_of_rounds=6)
     partial = cipher.cipher_partial_inverse(5, 5, True)
@@ -165,6 +170,7 @@ def partial_subkey_recovery_vectorized():
     print("Partial subkey6 recovery ...")
     # full_subkey6 = 152108258343 <=> partial_subkey6 (30 bits that influences the l.a) = 138149613607 <=> i = 44658535
     # Reduction of the research area:
+    Juan = {} # keys of this dict are the 30 guessed bits of subkey6, the values are the corresponding counters
     for i in range(44658533, 44658538): # range(pow(2,34)) for the full research
         partial_subkey = gen_partial_subkey(i)
         np_partial_subkey = np.frombuffer(int(partial_subkey).to_bytes(length=8, byteorder='big'), dtype=np.uint8).reshape(-1, 1)
@@ -176,18 +182,18 @@ def partial_subkey_recovery_vectorized():
         for index, plaintext in enumerate(dictio_using_key.keys()):
             if test_linear_approximation_on_a_pair(int(plaintext,16), int(state64_list[index],16)):
                 count += 1
-        bias = abs(float(count) - nb_pairs/2)/nb_pairs
+        bias = abs(float(count)/nb_pairs - 1/2)
         if bias > max_bias:
             max_bias = bias
             true_partial_subkey = partial_subkey
-        # max = bias
+        Juan[partial_subkey] = count
         count = 0
     end_time = datetime.now()
     print('Duration: {}'.format(end_time - start_time))
     print("Partial subkey6 = {}".format(hex(true_partial_subkey)))
     print("-------------")
 
-    return true_partial_subkey
+    return true_partial_subkey, Juan
 
 
 def partial_subkey_recovery():
@@ -214,13 +220,11 @@ def partial_subkey_recovery():
             state64 = partial.evaluate([partial_subkey, ciphertext]) # TODO: check if order is correct
             if test_linear_approximation_on_a_pair(plaintext, state64):
                 count += 1
-        # bias = abs(float(count) - nb_pairs/2)/nb_pairs
         bias = abs(float(count)/nb_pairs - 1/2)
         print(f"bias = {bias}")
         if bias > max_bias:
             max_bias = bias
             true_partial_subkey = partial_subkey
-        # max = bias
         print(f"count = {count}")
         count = 0
     end_time = datetime.now()
@@ -233,11 +237,19 @@ def partial_subkey_recovery():
 
 # Symbolic master key
 symbolic_master_key = ["k" + str(i) for i in range(64)]
-# print("6th round subkey:")
+symbolic_key = symbolic_master_key
 subkey6 = ['k2', 'k43', 'k26', 'k16', 'k41', 'k9', 'k25', 'k49', 'k59', 'k1', 'k40', 'k34', 'k24', 'k56', 'k18', 'k17', 'k0',
       'k50', 'k51', 'k58', 'k57', 'k48', 'k10', 'k33', 'k12', 'k22', 'k29', 'k44', 'k62', 'k61', 'k37', 'k20', 'k30',
       'k11', 'k13', 'k54', 'k19', 'k46', 'k28', 'k53', 'k5', 'k14', 'k3', 'k4', 'k38', 'k52', 'k45', 'k21']
-symbolic_key = symbolic_master_key
+# Subkeys in function of the bits of the masterkey:
+Cs = {
+1 : ['k9', 'k50', 'k33', 'k59', 'k48', 'k16', 'k32', 'k56', 'k1', 'k8', 'k18', 'k41', 'k2', 'k34', 'k25', 'k24', 'k43', 'k57', 'k58', 'k0', 'k35', 'k26', 'k17', 'k40', 'k21', 'k27', 'k38', 'k53', 'k36', 'k3', 'k46', 'k29', 'k4', 'k52', 'k22', 'k28', 'k60', 'k20', 'k37', 'k62', 'k14', 'k19', 'k44', 'k13', 'k12', 'k61', 'k54', 'k30'],
+2 : ['k1', 'k42', 'k25', 'k51', 'k40', 'k8', 'k24', 'k48', 'k58', 'k0', 'k10', 'k33', 'k59', 'k26', 'k17', 'k16', 'k35', 'k49', 'k50', 'k57', 'k56', 'k18', 'k9', 'k32', 'k13', 'k19', 'k30', 'k45', 'k28', 'k62', 'k38', 'k21', 'k27', 'k44', 'k14', 'k20', 'k52', 'k12', 'k29', 'k54', 'k6', 'k11', 'k36', 'k5', 'k4', 'k53', 'k46', 'k22'],
+3 : ['k50', 'k26', 'k9', 'k35', 'k24', 'k57', 'k8', 'k32', 'k42', 'k49', 'k59', 'k17', 'k43', 'k10', 'k1', 'k0', 'k48', 'k33', 'k34', 'k41', 'k40', 'k2', 'k58', 'k16', 'k60', 'k3', 'k14', 'k29', 'k12', 'k46', 'k22', 'k5', 'k11', 'k28', 'k61', 'k4', 'k36', 'k27', 'k13', 'k38', 'k53', 'k62', 'k20', 'k52', 'k19', 'k37', 'k30', 'k6'],
+4 : ['k34', 'k10', 'k58', 'k48', 'k8', 'k41', 'k57', 'k16', 'k26', 'k33', 'k43', 'k1', 'k56', 'k59', 'k50', 'k49', 'k32', 'k17', 'k18', 'k25', 'k24', 'k51', 'k42', 'k0', 'k44', 'k54', 'k61', 'k13', 'k27', 'k30', 'k6', 'k52', 'k62', 'k12', 'k45', 'k19', 'k20', 'k11', 'k60', 'k22', 'k37', 'k46', 'k4', 'k36', 'k3', 'k21', 'k14', 'k53'],
+5 : ['k18', 'k59', 'k42', 'k32', 'k57', 'k25', 'k41', 'k0', 'k10', 'k17', 'k56', 'k50', 'k40', 'k43', 'k34', 'k33', 'k16', 'k1', 'k2', 'k9', 'k8', 'k35', 'k26', 'k49', 'k28', 'k38', 'k45', 'k60', 'k11', 'k14', 'k53', 'k36', 'k46', 'k27', 'k29', 'k3', 'k4', 'k62', 'k44', 'k6', 'k21', 'k30', 'k19', 'k20', 'k54', 'k5', 'k61', 'k37'],
+6 : ['k2', 'k43', 'k26', 'k16', 'k41', 'k9', 'k25', 'k49', 'k59', 'k1', 'k40', 'k34', 'k24', 'k56', 'k18', 'k17', 'k0', 'k50', 'k51', 'k58', 'k57', 'k48', 'k10', 'k33', 'k12', 'k22', 'k29', 'k44', 'k62', 'k61', 'k37', 'k20', 'k30', 'k11', 'k13', 'k54', 'k19', 'k46', 'k28', 'k53', 'k5', 'k14', 'k3', 'k4', 'k38', 'k52', 'k45', 'k21'],
+}
 
 def partial_master_key_recovery(val_int, symbolic_key, subkey6):
     """
@@ -282,7 +294,7 @@ def gen_rest_of_masterkey(i, unknown_bits_position):
 def master_key_recovery():
     """
     from claasp.cipher_modules.linear_key_recovery_DES import *
-    master_key_recovery()
+    Paul = master_key_recovery()
     """
     # partial_subkey = partial_subkey_recovery_vectorized()
     partial_subkey = 138149613607 # to be removed
@@ -307,6 +319,8 @@ def master_key_recovery():
     start_time = datetime.now()
     # full_masterkey = 1166834735692856138 <=> partial_masterkey (34 remaining bits) = 387030374883592 <=> i = 46158058
     # Reduction of the research area:
+    all_guessed_master_key = {}
+    flag_masterkey_found = 0
     for i in range(46158055, 46158060): # range(pow(2,34)) for the full research
         guess_rest_of_partial_masterkey = gen_rest_of_masterkey(i, unknown_bits_position)
         guessed_master_key = guess_rest_of_partial_masterkey ^ partial_masterkey
@@ -314,13 +328,16 @@ def master_key_recovery():
         if ciphertext == ciphertext_from_guessed_master_key:
             end_time = datetime.now()
             print('Duration: {}'.format(end_time - start_time))
-            return guessed_master_key
+            all_guessed_master_key[guessed_master_key] = True
+            # return guessed_master_key
+            flag_masterkey_found = 1
+            break
+        all_guessed_master_key[guessed_master_key] = False
     end_time = datetime.now()
     print('Duration: {}'.format(end_time - start_time))
-    print("Master key not found")
-
-
-
-# pos: 000000 111111 000000 111111 111111 111111 000000 111111
-# val: 000000 000010 000000 101010 010110 111101 000000 100111
-# 0000 0011 1111 0000 0011 1111 1111 1111 1111 0000 0011 1111
+    if not flag_masterkey_found:
+        print("Master key not found")
+    else:
+        print("Master key found !!!")
+    Paul = [all_guessed_master_key, partial_masterkey, unknown_bits_position]
+    return Paul
