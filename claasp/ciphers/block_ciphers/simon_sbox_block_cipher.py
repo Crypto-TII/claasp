cb470d45d8129412cd442279933de1bc88e4327a
Alessandro De Piccoli <alessandro.depiccoli@unimi.it>
2024-07-03 11:30:50 +0200

    Optimize Simon Sbox version

--- claasp/ciphers/block_ciphers/simon_sbox_block_cipher.py
+++ claasp/ciphers/block_ciphers/simon_sbox_block_cipher.py
@@ -32,20 +32,40 @@ PARAMETERS_CONFIGURATION_LIST = [
     {'block_bit_size': 128, 'key_bit_size': 192, 'number_of_rounds': 69},
     {'block_bit_size': 128, 'key_bit_size': 256, 'number_of_rounds': 72}
 ]
-z = [
+Z = [
     4506230155203752166,
     2575579794259089498,
     3160415496042964403,
     3957284701066611983,
     3781244162168104175
 ]
-wordsize2zindex = {
+WORDSIZE_TO_ZINDEX = {
     16: {4: 0},
     24: {3: 0, 4: 1},
     32: {3: 2, 4: 3},
     48: {2: 2, 3: 3},
     64: {2: 2, 3: 3, 4: 4}
 }
+SBOX = [0, 0, 0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 0, 0, 0, 1, 0, 0, 2, 3, 8, 8, 8, 9, 12, 12, 14, 15, 0, 0,
+        0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 16, 16, 16, 17, 16, 16, 18, 19, 24, 24, 24, 25, 28, 28, 30,
+        31, 0, 0, 0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 0, 0, 0, 1, 0, 0, 2, 3, 8, 8, 8, 9, 12, 12, 14, 15,
+        32, 32, 32, 33, 32, 32, 34, 35, 32, 32, 32, 33, 36, 36, 38, 39, 48, 48, 48, 49, 48, 48, 50, 51, 56, 56,
+        56, 57, 60, 60, 62, 63, 0, 0, 0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 0, 0, 0, 1, 0, 0, 2, 3, 8, 8, 8,
+        9, 12, 12, 14, 15, 0, 0, 0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 16, 16, 16, 17, 16, 16, 18, 19, 24,
+        24, 24, 25, 28, 28, 30, 31, 64, 64, 64, 65, 64, 64, 66, 67, 64, 64, 64, 65, 68, 68, 70, 71, 64, 64, 64,
+        65, 64, 64, 66, 67, 72, 72, 72, 73, 76, 76, 78, 79, 96, 96, 96, 97, 96, 96, 98, 99, 96, 96, 96, 97, 100,
+        100, 102, 103, 112, 112, 112, 113, 112, 112, 114, 115, 120, 120, 120, 121, 124, 124, 126, 127, 0, 0, 0,
+        1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 0, 0, 0, 1, 0, 0, 2, 3, 8, 8, 8, 9, 12, 12, 14, 15, 0, 0, 0, 1,
+        0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 16, 16, 16, 17, 16, 16, 18, 19, 24, 24, 24, 25, 28, 28, 30, 31, 0,
+        0, 0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 0, 0, 0, 1, 0, 0, 2, 3, 8, 8, 8, 9, 12, 12, 14, 15, 32, 32,
+        32, 33, 32, 32, 34, 35, 32, 32, 32, 33, 36, 36, 38, 39, 48, 48, 48, 49, 48, 48, 50, 51, 56, 56, 56, 57,
+        60, 60, 62, 63, 128, 128, 128, 129, 128, 128, 130, 131, 128, 128, 128, 129, 132, 132, 134, 135, 128,
+        128, 128, 129, 128, 128, 130, 131, 136, 136, 136, 137, 140, 140, 142, 143, 128, 128, 128, 129, 128, 128,
+        130, 131, 128, 128, 128, 129, 132, 132, 134, 135, 144, 144, 144, 145, 144, 144, 146, 147, 152, 152, 152,
+        153, 156, 156, 158, 159, 192, 192, 192, 193, 192, 192, 194, 195, 192, 192, 192, 193, 196, 196, 198, 199,
+        192, 192, 192, 193, 192, 192, 194, 195, 200, 200, 200, 201, 204, 204, 206, 207, 224, 224, 224, 225, 224,
+        224, 226, 227, 224, 224, 224, 225, 228, 228, 230, 231, 240, 240, 240, 241, 240, 240, 242, 243, 248, 248,
+        248, 249, 252, 252, 254, 255]
 
 
 class SimonSboxBlockCipher(Cipher):
@@ -60,7 +80,7 @@ class SimonSboxBlockCipher(Cipher):
     - ``key_bit_size`` -- **integer** (default: `64`); cipher key bit size of the cipher
     - ``number_of_rounds`` -- **integer** (default: `None`); number of rounds of the cipher. The cipher uses the
       corresponding amount given the other parameters (if available) when number_of_rounds is None
-    - ``rotation_amount`` -- **list** (default: `[-1, -8, -2]`); the list containing the 3 rotation amounts for the
+    - ``rotation_amount`` -- **tuple** (default: `(-1, -8, -2)`); the tuple containing the 3 rotation amounts for the
       round function
 
     EXAMPLES::
@@ -74,45 +94,15 @@ class SimonSboxBlockCipher(Cipher):
         'intermediate_output_0_0'
     """
 
-    sbox = [0, 0, 0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 0, 0, 0, 1, 0, 0, 2, 3, 8, 8, 8, 9, 12, 12, 14, 15, 0, 0,
-            0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 16, 16, 16, 17, 16, 16, 18, 19, 24, 24, 24, 25, 28, 28, 30,
-            31, 0, 0, 0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 0, 0, 0, 1, 0, 0, 2, 3, 8, 8, 8, 9, 12, 12, 14, 15,
-            32, 32, 32, 33, 32, 32, 34, 35, 32, 32, 32, 33, 36, 36, 38, 39, 48, 48, 48, 49, 48, 48, 50, 51, 56, 56,
-            56, 57, 60, 60, 62, 63, 0, 0, 0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 0, 0, 0, 1, 0, 0, 2, 3, 8, 8, 8,
-            9, 12, 12, 14, 15, 0, 0, 0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 16, 16, 16, 17, 16, 16, 18, 19, 24,
-            24, 24, 25, 28, 28, 30, 31, 64, 64, 64, 65, 64, 64, 66, 67, 64, 64, 64, 65, 68, 68, 70, 71, 64, 64, 64,
-            65, 64, 64, 66, 67, 72, 72, 72, 73, 76, 76, 78, 79, 96, 96, 96, 97, 96, 96, 98, 99, 96, 96, 96, 97, 100,
-            100, 102, 103, 112, 112, 112, 113, 112, 112, 114, 115, 120, 120, 120, 121, 124, 124, 126, 127, 0, 0, 0,
-            1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 0, 0, 0, 1, 0, 0, 2, 3, 8, 8, 8, 9, 12, 12, 14, 15, 0, 0, 0, 1,
-            0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 16, 16, 16, 17, 16, 16, 18, 19, 24, 24, 24, 25, 28, 28, 30, 31, 0,
-            0, 0, 1, 0, 0, 2, 3, 0, 0, 0, 1, 4, 4, 6, 7, 0, 0, 0, 1, 0, 0, 2, 3, 8, 8, 8, 9, 12, 12, 14, 15, 32, 32,
-            32, 33, 32, 32, 34, 35, 32, 32, 32, 33, 36, 36, 38, 39, 48, 48, 48, 49, 48, 48, 50, 51, 56, 56, 56, 57,
-            60, 60, 62, 63, 128, 128, 128, 129, 128, 128, 130, 131, 128, 128, 128, 129, 132, 132, 134, 135, 128,
-            128, 128, 129, 128, 128, 130, 131, 136, 136, 136, 137, 140, 140, 142, 143, 128, 128, 128, 129, 128, 128,
-            130, 131, 128, 128, 128, 129, 132, 132, 134, 135, 144, 144, 144, 145, 144, 144, 146, 147, 152, 152, 152,
-            153, 156, 156, 158, 159, 192, 192, 192, 193, 192, 192, 194, 195, 192, 192, 192, 193, 196, 196, 198, 199,
-            192, 192, 192, 193, 192, 192, 194, 195, 200, 200, 200, 201, 204, 204, 206, 207, 224, 224, 224, 225, 224,
-            224, 226, 227, 224, 224, 224, 225, 228, 228, 230, 231, 240, 240, 240, 241, 240, 240, 242, 243, 248, 248,
-            248, 249, 252, 252, 254, 255]
-
-    def __init__(self, block_bit_size=32, key_bit_size=64, number_of_rounds=None, rotation_amounts=[-1, -8, -2]):
+    def __init__(self, block_bit_size=32, key_bit_size=64, number_of_rounds=32, rotation_amounts=(-1, -8, -2)):
         self.block_bit_size = block_bit_size
+        self.key_bit_size = key_bit_size
         self.word_size = self.block_bit_size // 2
-        self.key_size_in_words = key_bit_size // self.word_size
-        self.rotation_amounts = rotation_amounts[:]
-        self.z = z[wordsize2zindex[self.word_size][self.key_size_in_words]]
+        self.number_of_key_words = key_bit_size // self.word_size
+        self.rotation_amounts = rotation_amounts
+        self.z = Z[WORDSIZE_TO_ZINDEX[self.word_size][self.number_of_key_words]]
         self.c = 2**self.word_size - 4
-        self.number_of_sboxes = self.block_bit_size // 16
-
-        if number_of_rounds is None:
-            for parameters in PARAMETERS_CONFIGURATION_LIST:
-                if parameters['block_bit_size'] == self.block_bit_size and \
-                        parameters['key_bit_size'] == key_bit_size:
-                    number_of_rounds = parameters['number_of_rounds']
-                    break
-
-            if number_of_rounds is None:
-                raise ValueError("No available number of rounds for the given parameters.")
+        self.number_of_sboxes = self.word_size // 8
 
         super().__init__(family_name="simon",
                          cipher_type="block_cipher",
@@ -133,14 +123,14 @@ class SimonSboxBlockCipher(Cipher):
         self.add_cipher_output_component([x[0], y[0]], [x[1], y[1]], self.block_bit_size)
 
     def f(self, x):
-        # f(x) = (x <<< 1) & (x <<< 8)
+        # f(x) = ((x <<< 1) & (x <<< 8)) ^ (x <<< 2)
         input_positions_pattern = (1, 8, 15, 22, 29, 36, 43, 50, 57)
         output_positions_pattern = (0, 7, 14, 21, 28, 35, 42, 49)
         output_ids = [""] * self.word_size
         output_positions = [0] * self.word_size
         for i in range(self.number_of_sboxes):
             sbox_input_positions = [(position + 8*i) % self.word_size for position in input_positions_pattern]
-            sbox_id = self.add_SBOX_component([x[0]], [sbox_input_positions], 8, self.sbox).id
+            sbox_id = self.add_SBOX_component([x[0]], [sbox_input_positions], 8, SBOX).id
             sbox_output_positions = [(position + 8*i) % self.word_size for position in output_positions_pattern]
             for j, sbox_output_position in enumerate(sbox_output_positions):
                 output_ids[sbox_output_position] = sbox_id
@@ -172,37 +162,40 @@ class SimonSboxBlockCipher(Cipher):
         return (new_x_id, list(range(self.word_size))), (x[0], x[1])
 
     def generate_round_key(self, round_keys, round_number):
-        if round_number < self.key_size_in_words:
-            key_size = self.key_size_in_words - round_number - 1
+        if round_number < self.number_of_key_words:
+            key_index = self.number_of_key_words - round_number - 1
 
             self.add_round_key_output_component(
                 [INPUT_KEY],
-                [list(range(self.word_size * key_size, self.word_size * (key_size + 1)))],
+                [list(range(self.word_size * key_index, self.word_size * (key_index + 1)))],
                 self.word_size).id
             round_keys[round_number] = \
-                INPUT_KEY, list(range(self.word_size * key_size, self.word_size * (key_size + 1)))
+                INPUT_KEY, list(range(self.word_size * key_index, self.word_size * (key_index + 1)))
 
         else:
-            i = round_number - self.key_size_in_words
+            constant_index = round_number - self.number_of_key_words
 
             # c ^ z[j][i]
             round_constant = self.add_constant_component(
-                self.word_size, self.c ^ ((self.z >> (61 - (i % 62))) & 1)).id
+                self.word_size, self.c ^ ((self.z >> (61 - (constant_index % 62))) & 1)).id
+            
+            s3_x_input_positions = [(i - 3) % self.word_size for i in range(self.word_size)]
+            s1_x_input_positions = [(i - 1) % self.word_size for i in range(self.word_size)]
+            s4_x_input_positions = [(i - 4) % self.word_size for i in range(self.word_size)]
 
-            op = self.add_rotate_component([round_keys[i + self.key_size_in_words - 1][0]],
-                                           [round_keys[i + self.key_size_in_words - 1][1]],
-                                           self.word_size, 3).id
-
-            if self.key_size_in_words == 4:
-                op = self.add_XOR_component([op, round_keys[i + 1][0]],
-                                            [list(range(self.word_size)), round_keys[i + 1][1]],
+            if self.number_of_key_words == 4:
+                op = self.add_XOR_component([round_keys[round_number - 1][0], round_keys[round_number - 3][0]],
+                                            [s3_x_input_positions, round_keys[round_number - 3][1]],
+                                            self.word_size).id
+                op = self.add_XOR_component([op, op], [s1_x_input_positions, list(range(self.word_size))],
+                                            self.word_size).id
+            else:
+                op = self.add_XOR_component([round_keys[round_number - 1][0], round_keys[round_number - 1][0]],
+                                            [s4_x_input_positions, s3_x_input_positions],
                                             self.word_size).id
 
-            rot_id = self.add_rotate_component([op], [list(range(self.word_size))], self.word_size, 1).id
-
-            xor_id = self.add_XOR_component([round_constant, round_keys[i][0], op, rot_id],
-                                            [list(range(self.word_size)), round_keys[i][1]] +
-                                            [list(range(self.word_size))] * 2,
+            xor_id = self.add_XOR_component([round_constant, op, round_keys[constant_index][0]],
+                                            [list(range(self.word_size))] * 2 + [round_keys[constant_index][1]],
                                             self.word_size).id
 
             self.add_round_key_output_component([xor_id], [list(range(self.word_size))], self.word_size).id
