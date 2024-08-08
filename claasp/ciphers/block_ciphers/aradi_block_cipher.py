4c899224457f99707fb2a5cb6f471406ecb6ebcb
DavidGerault <49142687+DavidGerault@users.noreply.github.com>
2024-08-08 18:35:04 +0400

    Update aradi_block_cipher.py
    FIX/Fixed aradi doctest

--- claasp/ciphers/block_ciphers/aradi_block_cipher.py
+++ claasp/ciphers/block_ciphers/aradi_block_cipher.py
@@ -45,7 +45,7 @@ class AradiBlockCipher(Cipher):
 
         sage: from claasp.ciphers.block_ciphers.aradi_block_cipher import AradiBlockCipher
         sage: aradi = AradiBlockCipher(number_of_rounds=16)
-        sage: hex(aradi.evaluate([0, 0x1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100]))
+        sage: aradi.evaluate([0, 0x1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100])
         0x3f09abf400e3bd7403260defb7c53912
     """
 
