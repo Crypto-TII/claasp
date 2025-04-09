c4a99bed4cb8d50db2477b9df6f6c73e4132304b
Alessandro De Piccoli <alessandro.depiccoli@unimi.it>
2025-04-09 22:30:40 +0200

    Reformat and lint PRESENT block cipher

--- claasp/ciphers/block_ciphers/present_block_cipher.py
+++ claasp/ciphers/block_ciphers/present_block_cipher.py
@@ -1,17 +1,16 @@
-
 # ****************************************************************************
 # Copyright 2023 Technology Innovation Institute
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ****************************************************************************
@@ -22,20 +21,28 @@ from claasp.utils.utils import extract_inputs
 from claasp.name_mappings import INPUT_PLAINTEXT, INPUT_KEY
 
 sbox = [12, 5, 6, 11, 9, 0, 10, 13, 3, 14, 15, 8, 4, 7, 1, 2]
+# fmt: off
 permutations = [
     0, 16, 32, 48, 1, 17, 33, 49, 2, 18, 34, 50, 3, 19, 35, 51,
     4, 20, 36, 52, 5, 21, 37, 53, 6, 22, 38, 54, 7, 23, 39, 55,
     8, 24, 40, 56, 9, 25, 41, 57, 10, 26, 42, 58, 11, 27, 43, 59,
     12, 28, 44, 60, 13, 29, 45, 61, 14, 30, 46, 62, 15, 31, 47, 63
 ]
+# fmt: on
 PARAMETERS_CONFIGURATION_LIST = [
-    {'key_bit_size': 80, 'number_of_rounds': 31},
-    {'key_bit_size': 128, 'number_of_rounds': 31},
+    {"key_bit_size": 80, "number_of_rounds": 31},
+    {"key_bit_size": 128, "number_of_rounds": 31},
 ]
 reference_code = f"""
 def present_encrypt(plaintext, key):
-    from claasp.utils.integer_functions import (wordlist_to_int, int_to_wordlist, wordlist_to_int,
-                                             bytearray_to_int, bytearray_to_wordlist, int_to_bytearray)
+    from claasp.utils.integer_functions import (
+        wordlist_to_int,
+        int_to_wordlist,
+        wordlist_to_int,
+        bytearray_to_int,
+        bytearray_to_wordlist,
+        int_to_bytearray,
+    )
 
     key_size = {{0}}
     rounds = {{1}}
@@ -59,22 +66,22 @@ def present_encrypt(plaintext, key):
         return state ^ rk
 
     def update_key_register(key_register, key_size, i):
-        #[k0, k1, ... , k78, k79] = [k61, k62, ... , k59, k60]
+        # [k0, k1, ... , k78, k79] = [k61, k62, ... , k59, k60]
         key_register[:] = key_register[61:] + key_register[:61]
 
-        #[k0, k1, k2, k3] = S[k0, k1, k2, k3]
+        # [k0, k1, k2, k3] = S[k0, k1, k2, k3]
         sbox_output = sbox[wordlist_to_int(key_register[:4], 1)]
         key_register[:4] = int_to_wordlist(sbox_output, 1, 4)
 
         if key_size == 80:
-            #[k60, k61, k62, k63, k64] = [k60, k61, k62, k63, k64] ^ i
+            # [k60, k61, k62, k63, k64] = [k60, k61, k62, k63, k64] ^ i
             key_register[60:65] = int_to_wordlist(wordlist_to_int(key_register[60:65], 1) ^ i, 1, 5)
         elif key_size == 128:
-            #[k4, k5, k6, k7] = S[k4, k5, k6, k7]
+            # [k4, k5, k6, k7] = S[k4, k5, k6, k7]
             sbox_output = sbox[wordlist_to_int(key_register[4:8], 1)]
             key_register[4:8] = int_to_wordlist(sbox_output, 1, 4)
 
-            #[k61, k62, k63, k64, k65] = [k61, k62, k63, k64, k65] ^ i
+            # [k61, k62, k63, k64, k65] = [k61, k62, k63, k64, k65] ^ i
             key_register[61:66] = int_to_wordlist(wordlist_to_int(key_register[61:66], 1) ^ i, 1, 5)
 
     if key_size != 128 and key_size != 80:
@@ -89,7 +96,7 @@ def present_encrypt(plaintext, key):
         state = add_round_key(state, rk)
         state = sbox_layer(state)
         state = permutation_layer(state)
-        update_key_register(key_register, key_size, i+1)
+        update_key_register(key_register, key_size, i + 1)
 
     rk = wordlist_to_int(key_register[:64], 1)
     state = add_round_key(state, rk)
@@ -129,8 +136,8 @@ class PresentBlockCipher(Cipher):
             n = None
 
             for parameters in PARAMETERS_CONFIGURATION_LIST:
-                if parameters['key_bit_size'] == self.key_bit_size:
-                    n = parameters['number_of_rounds']
+                if parameters["key_bit_size"] == self.key_bit_size:
+                    n = parameters["number_of_rounds"]
                     break
 
             if n is None:
@@ -138,11 +145,14 @@ class PresentBlockCipher(Cipher):
         else:
             n = number_of_rounds
 
-        super().__init__(family_name="present", cipher_type="block_cipher",
-                         cipher_inputs=[INPUT_PLAINTEXT, INPUT_KEY],
-                         cipher_inputs_bit_size=[self.block_bit_size, self.key_bit_size],
-                         cipher_output_bit_size=self.block_bit_size,
-                         cipher_reference_code=reference_code.format(self.key_bit_size, n))
+        super().__init__(
+            family_name="present",
+            cipher_type="block_cipher",
+            cipher_inputs=[INPUT_PLAINTEXT, INPUT_KEY],
+            cipher_inputs_bit_size=[self.block_bit_size, self.key_bit_size],
+            cipher_output_bit_size=self.block_bit_size,
+            cipher_reference_code=reference_code.format(self.key_bit_size, n),
+        )
 
         data = [INPUT_PLAINTEXT], [list(range(self.block_bit_size))]
         key = [INPUT_KEY], [list(range(self.key_bit_size))]
@@ -176,10 +186,10 @@ class PresentBlockCipher(Cipher):
         return [new_data_id], [list(range(self.block_bit_size))]
 
     def sbox_layer(self, data):
-        sbox_output = [''] * 16
+        sbox_output = [""] * 16
 
         for i in range(16):
-            sbox_output[i] = self.add_SBOX_component(data[0], [data[1][0][i * 4:(i + 1) * 4]], 4, sbox).id
+            sbox_output[i] = self.add_SBOX_component(data[0], [data[1][0][i * 4 : (i + 1) * 4]], 4, sbox).id
 
         return sbox_output, [list(range(4))] * 16
 
@@ -194,8 +204,13 @@ class PresentBlockCipher(Cipher):
             xor = self.add_XOR_component([rot, constant_id], [list(range(60, 65)), list(range(3, 8))], 5).id
             return [sbox_1, rot, xor, rot], [list(range(4)), list(range(4, 60)), list(range(5)), list(range(65, 80))]
 
-        elif self.key_bit_size == 128:
+        if self.key_bit_size == 128:
             xor = self.add_XOR_component([rot, constant_id], [list(range(61, 66)), list(range(3, 8))], 5).id
             sbox_2 = self.add_SBOX_component([rot], [list(range(4, 8))], 4, sbox).id
-            return [sbox_1, sbox_2, rot, xor, rot], [list(range(4)), list(range(4)), list(range(8, 61)),
-                                                     list(range(5)), list(range(66, 128))]
+            return [sbox_1, sbox_2, rot, xor, rot], [
+                list(range(4)),
+                list(range(4)),
+                list(range(8, 61)),
+                list(range(5)),
+                list(range(66, 128)),
+            ]
