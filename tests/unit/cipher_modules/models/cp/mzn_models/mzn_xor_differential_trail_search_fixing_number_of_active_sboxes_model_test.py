65fb2d1b24ffb088ea38485e5559a2e350876dc7
simopez <simonepelizzola@gmail.com>
2025-05-23 10:45:23 +0200

    Fixed tests

--- tests/unit/cipher_modules/models/cp/mzn_models/mzn_xor_differential_trail_search_fixing_number_of_active_sboxes_model_test.py
+++ tests/unit/cipher_modules/models/cp/mzn_models/mzn_xor_differential_trail_search_fixing_number_of_active_sboxes_model_test.py
@@ -28,10 +28,10 @@ def test_find_lowest_weight_xor_differential_trail():
     assert solution['model_type'] == 'xor_differential'
     assert solution['solver_name'] == 'Chuffed'
     assert solution['total_weight'] == '30.0'
-    assert solution['components_values']['key'] == {'value': '00000000000000000000000000000000', 'weight': 0}
-    assert eval('0x' + solution['components_values']['plaintext']['value']) > 0
+    assert solution['components_values']['key'] == {'value': '0x00000000000000000000000000000000', 'weight': 0}
+    assert eval(solution['components_values']['plaintext']['value']) > 0
     assert solution['components_values']['plaintext']['weight'] == 0
-    assert eval('0x' + solution['components_values']['cipher_output_1_32']['value']) >= 0
+    assert eval(solution['components_values']['cipher_output_1_32']['value']) >= 0
     assert solution['components_values']['cipher_output_1_32']['weight'] == 0
 
 
@@ -46,7 +46,7 @@ def test_find_one_xor_differential_trail():
     assert solution['model_type'] == 'xor_differential'
     assert solution['solver_name'] == 'Chuffed'
     assert eval(solution['total_weight']) >= 0.0
-    assert solution['components_values']['key'] == {'value': '00000000000000000000000000000000', 'weight': 0}
+    assert solution['components_values']['key'] == {'value': '0x00000000000000000000000000000000', 'weight': 0}
     assert solution['components_values']['plaintext']['weight'] == 0
 
 
@@ -62,7 +62,7 @@ def test_find_one_xor_differential_trail_with_fixed_weight():
     assert solution['solver_name'] == 'Chuffed'
     assert eval(solution['total_weight']) == 224.0
     assert solution['components_values']['key'] == {'value': '00000000000000000000000000000000', 'weight': 0}
-    assert eval('0x' + solution['components_values']['plaintext']['value']) > 0
+    assert eval(solution['components_values']['plaintext']['value']) > 0
     assert solution['components_values']['plaintext']['weight'] == 0
     assert solution['components_values']['cipher_output_1_32']['weight'] == 0
 
@@ -75,10 +75,10 @@ def test_solve_full_two_steps_xor_differential_model():
     constraints = mzn.solve_full_two_steps_xor_differential_model('xor_differential_one_solution', -1, fixed_variables, 'Chuffed', 'Chuffed')
 
     assert str(constraints['cipher']) == 'aes_block_cipher_k128_p128_o128_r2'
-    assert eval('0x' + constraints['components_values']['intermediate_output_0_35']['value']) >= 0
+    assert eval(constraints['components_values']['intermediate_output_0_35']['value']) >= 0
     assert constraints['components_values']['intermediate_output_0_35']['weight'] == 0
-    assert eval('0x' + constraints['components_values']['xor_0_36']['value']) >= 0
+    assert eval(constraints['components_values']['xor_0_36']['value']) >= 0
     assert constraints['components_values']['xor_0_36']['weight'] == 0
-    assert eval('0x' +  constraints['components_values']['intermediate_output_0_37']['value']) >= 0
+    assert eval(constraints['components_values']['intermediate_output_0_37']['value']) >= 0
     assert constraints['components_values']['intermediate_output_0_37']['weight'] == 0
     assert eval(constraints['total_weight']) >= 0
