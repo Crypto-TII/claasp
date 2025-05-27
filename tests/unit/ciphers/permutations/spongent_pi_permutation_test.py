7ea6d91b6494cd6c189ffde7f330f6b665aad22a
sharwantiwari <shrawant@gmail.com>
2025-05-27 16:01:55 +0200

    Reduce memory usage in spongent_pi_permutation pytest

--- tests/unit/ciphers/permutations/spongent_pi_permutation_test.py
+++ tests/unit/ciphers/permutations/spongent_pi_permutation_test.py
@@ -14,15 +14,12 @@ def test_spongent_pi_permutation():
     assert spongentpi.id == 'spongent_pi_p160_o160_r4'
     assert spongentpi.component_from(3, 0).id == 'linear_layer_3_0'
 
-    # Very long test
-    spongentpi = SpongentPiPermutation()
     plaintext = 0x0000000000000000000000000000000000000000
-    ciphertext = 0xcaed745fb9d13ede0ec562a18682cba286000ce8
-    assert spongentpi.evaluate([plaintext]) == ciphertext
+    ciphertext = 0x60a224efe52f9f1febbfe51f7a2dc9564341167
+    assert hex(spongentpi.evaluate([plaintext])) == hex(ciphertext)
 
-    # Very long test
-    spongentpi = SpongentPiPermutation(state_bit_size=176, number_of_rounds=90)
+    spongentpi = SpongentPiPermutation(state_bit_size=176, number_of_rounds=4)
     plaintext = 0x0123456789abcdef0123456789abcdef0123456789ab
-    ciphertext = 0x04adf4b51546dc10694325ff73b1352f141d8023da08
+    ciphertext = 0x8675478f97cafe723bf668c5e573ae9b582131499660
     assert spongentpi.evaluate([plaintext]) == ciphertext
     assert spongentpi.evaluate_vectorized([plaintext], evaluate_api=True) == ciphertext
