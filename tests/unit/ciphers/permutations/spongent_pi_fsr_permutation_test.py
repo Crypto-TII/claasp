9ed1622a5e5de44b7689ec02af384bebdd0c61ee
sharwantiwari <shrawant@gmail.com>
2025-05-27 15:41:10 +0200

    Reduce memory usage inspongent_pi_fsr_permutation pytest

--- tests/unit/ciphers/permutations/spongent_pi_fsr_permutation_test.py
+++ tests/unit/ciphers/permutations/spongent_pi_fsr_permutation_test.py
@@ -2,27 +2,10 @@ from claasp.ciphers.permutations.spongent_pi_fsr_permutation import SpongentPiFS
 
 
 def test_spongent_pi_fsr_permutation():
-    spongentpi = SpongentPiFSRPermutation()
-    assert spongentpi.family_name == 'spongent_pi_fsr'
-    assert spongentpi.type == 'permutation'
-    assert spongentpi.number_of_rounds == 80
-    assert spongentpi.id == 'spongent_pi_fsr_p160_o160_r80'
-    assert spongentpi.component_from(0, 0).id == 'constant_0_0'
-
-    spongentpi = SpongentPiFSRPermutation(state_bit_size=160, number_of_rounds=4)
-    assert spongentpi.number_of_rounds == 4
-    assert spongentpi.id == 'spongent_pi_fsr_p160_o160_r4'
+    spongentpi = SpongentPiFSRPermutation(state_bit_size=160, number_of_rounds=8)
+    assert spongentpi.number_of_rounds == 8
+    assert spongentpi.id == 'spongent_pi_fsr_p160_o160_r8'
     assert spongentpi.component_from(3, 0).id == 'fsr_3_0'
-
-    # Very long test
-    spongentpi = SpongentPiFSRPermutation()
     plaintext = 0x0000000000000000000000000000000000000000
-    ciphertext = 0xcaed745fb9d13ede0ec562a18682cba286000ce8
+    ciphertext = 0x8ec029fbad1689d63bb43a488a58c957885d3d8a
     assert spongentpi.evaluate([plaintext]) == ciphertext
-
-    # Very long test
-    spongentpi = SpongentPiFSRPermutation(state_bit_size=176, number_of_rounds=90)
-    plaintext = 0x0123456789abcdef0123456789abcdef0123456789ab
-    ciphertext = 0x04adf4b51546dc10694325ff73b1352f141d8023da08
-    assert spongentpi.evaluate([plaintext]) == ciphertext
-    #assert spongentpi.evaluate_vectorized([plaintext], evaluate_api=True) == ciphertext
