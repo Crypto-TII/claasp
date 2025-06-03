a05cc605d0ca12d04a843b8cac0ff146e2aac161
rocco.brunelli <rocco.brunelli@uniroma3.it>
2025-06-03 12:20:15 +0200

    - in the file /mzn_hadipour_boomerang_model.py I - create one big cipher with the components of upper - and lower together, overlapping the midde rounds - but with the right prefix - Note that, even if eme1 is the inverse I update - the big cipher following the straight direction - So the rounds are correctly increasing also for - eme1

--- claasp/cipher_modules/models/cp/mzn_models/mzn_hadipour_boomerang_model.py
+++ claasp/cipher_modules/models/cp/mzn_models/mzn_hadipour_boomerang_model.py
@@ -29,40 +29,86 @@ class MznHadipourBoomerangModel(MznModel):
         total_number_of_rounds = self.top_part_number_of_rounds + self.middle_part_number_of_rounds + self.bottom_part_number_of_rounds
         assert total_number_of_rounds == cipher.number_of_rounds
 
-        e0_cipher = cipher.get_partial_cipher(
+        e0em_cipher = cipher.get_partial_cipher(
             start_round=0,
             end_round=self.top_part_number_of_rounds + self.middle_part_number_of_rounds - 1,
             keep_key_schedule=False
         )
-        e1_cipher = cipher.cipher_partial_inverse(
+        e0em_cipher.add_prefix('upper_')
+
+        e0em_cipher.print_as_python_dictionary()
+
+        eme1_cipher = cipher.cipher_partial_inverse(
             start_round=self.top_part_number_of_rounds,
             end_round=self.top_part_number_of_rounds + self.middle_part_number_of_rounds + self.bottom_part_number_of_rounds - 1,
             keep_key_schedule=False
         )
-        # TODO:: Create a unified cipher from e0_cipher and e1_cipher
-        unified_cipher = None
-        super().__init__(unified_cipher)
+
+        eme1_cipher.add_prefix('lower_')
+
+        for i in range(0, self.middle_part_number_of_rounds):
+            print(f"len of e0em before {len(e0em_cipher._rounds.rounds[self.top_part_number_of_rounds + i]._components)}")
+            print(f"rounds of e0em: {self.top_part_number_of_rounds + i}")
+            print(f"rounds of eme1: {self.bottom_part_number_of_rounds + self.middle_part_number_of_rounds -i}")
+            e0em_cipher._rounds.rounds[self.top_part_number_of_rounds + i]._components.extend(eme1_cipher._rounds.rounds[self.bottom_part_number_of_rounds +
+                                                                                                                        self.middle_part_number_of_rounds -i -1]._components)
+            print(f"len of e0em after {len(e0em_cipher._rounds.rounds[self.top_part_number_of_rounds + i]._components)}")
+        
+        ## add also the last part of e1
+        for i in range(0, self.bottom_part_number_of_rounds):
+            e0em_cipher._rounds.rounds[self.top_part_number_of_rounds + self.middle_part_number_of_rounds + i]._components.extend(eme1_cipher._rounds.rounds[self.bottom_part_number_of_rounds 
+                                                                                                                                                             - i - 1]._components)
+
+        e0em_cipher.print_as_python_dictionary()
+
+        # cipher._rounds.rounds[round_number]._components.extend([regular_component_copy])
 
         import ipdb;
         ipdb.set_trace()
+
+        
+
+
+        # TODO:: Create a unified cipher from e0em_cipher and eme1_cipher
+        unified_cipher = None
+        super().__init__(unified_cipher)
+
+        
 
     def build_hadipour_boomerang_model(self, weight=-1):
         top_part_components = []
         middle_part_components = []
         bottom_part_components = []
 
+        # creation of e0
+        self.cipher.add_prefix('upper_')
+        e0_rounds = [0, self.top_part_number_of_rounds] # the actual rounds of e0
+        e0_number_of_rounds = self.top_part_number_of_rounds # the number of rounds of e0
+        for round_number in range(e0_rounds[0], e0_rounds[1]):
+            top_part_components.extend(self.cipher.get_components_in_round(round_number))
+        self.cipher.remove_prefix('upper_')
+
+        # creation of em
+        self.cipher.add_prefix('middle_')
+        em_rounds = [e0_rounds[0], e0_rounds[1] + self.middle_part_number_of_rounds] # the actual rounds of em
+        em_number_of_rounds = self.middle_part_number_of_rounds # the number of rounds of em
+        for round_number in range(em_rounds[0], em_rounds[1]):
+            middle_part_components.extend(self.cipher.get_components_in_round(round_number))
+        self.cipher.remove_prefix('middle_')
+         
+        # creation of e1
+        self.cipher.add_prefix('lower_')
+        e1_rounds = [em_rounds[1], em_rounds[1] + self.bottom_part_number_of_rounds]
+        e1_number_of_rounds = self.bottom_part_number_of_rounds
+        for round_number in range(e1_rounds[0], e1_rounds[1]):
+            bottom_part_components.extend(self.cipher.get_components_in_round(round_number))
+        self.cipher.remove_prefix('lower_')
+
         # by default we are defining every component to have a pure differential modeling
         component_and_model_types = _generate_component_model_types(
             self.cipher,
             model_type="cp_xor_differential_propagation_constraints"
         )
-        for round_number in range(0, self.top_part_number_of_rounds):
-            top_part_components.extend(self.cipher.get_components_in_round(round_number))
-        e0_number_of_rounds = self.top_part_number_of_rounds + self.middle_part_number_of_rounds
-        for round_number in range(self.top_part_number_of_rounds, e0_number_of_rounds):
-            middle_part_components.extend(self.cipher.get_components_in_round(round_number))
-        for round_number in range(e0_number_of_rounds, e0_number_of_rounds + self.bottom_part_number_of_rounds):
-            bottom_part_components.extend(self.cipher.get_components_in_round(round_number))
 
         # updating the type of modeling only for the middle part
         _set_model_type_for_components(
@@ -70,6 +116,9 @@ class MznHadipourBoomerangModel(MznModel):
             middle_part_components,
             model_type="cp_deterministic_truncated_xor_differential_constraints"
         )
+
+        # I would create a genere mzn model for boomerang attack, so we can take in input the three parts and modify them
+        # according their prefix
         self.build_generic_mzn_model_from_dictionary(component_and_model_types)
 
         # TODO:: Add Hadipour constraints
