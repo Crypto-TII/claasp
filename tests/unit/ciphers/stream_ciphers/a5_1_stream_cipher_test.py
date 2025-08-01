f3a5fc82fe8a9ab5ca71844c16210da1fb68c440
mattiaformenti <mattia.formenti@tii.ae>
2025-08-01 08:12:11 +0400

    fix a51 and a52 unit tests

--- tests/unit/ciphers/stream_ciphers/a5_1_stream_cipher_test.py
+++ tests/unit/ciphers/stream_ciphers/a5_1_stream_cipher_test.py
@@ -1,15 +1,15 @@
-from claasp.ciphers.stream_ciphers.a5_2_stream_cipher import A52StreamCipher
+from claasp.ciphers.stream_ciphers.a5_1_stream_cipher import A51StreamCipher
 
-def test_a52():
-    a52 = A52StreamCipher()
-    assert a52.family_name == 'a52'
-    assert a52.type == 'stream_cipher'
-    assert a52.number_of_rounds == 229
-    assert a52.id == 'a52_k64_i22_o228_r229'
-    assert a52.component_from(0, 0).id == 'constant_0_0'
-    assert a52.component_from(1, 0).id == 'fsr_1_0'
+def test_a51():
+    a51 = A51StreamCipher()
+    assert a51.family_name == 'a51'
+    assert a51.type == 'stream_cipher'
+    assert a51.number_of_rounds == 229
+    assert a51.id == 'a51_k64_i22_o228_r229'
+    assert a51.component_from(0, 0).id == 'constant_0_0'
+    assert a51.component_from(1, 0).id == 'fsr_1_0'
 
-    key = 0x003fffffffffffff
-    frame = 0b1000010000000000000000
-    keystream = 0xf4512cac13593764460b722dadd51200350ca385a853735ee5c889944
-    assert a52.evaluate([key, frame]) == keystream
+    key = 0x48c4a2e691d5b3f7
+    frame = 0b0010110010000000000000
+    keystream = 0x534eaa582fe8151ab6e1855a728c093f4d68d757ed949b4cbe41b7c6b
+    assert a51.evaluate([key, frame]) == keystream
