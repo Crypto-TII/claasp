008e0db14d2d73841c8457422624e0d39f37fbd0
Francesco Vago <fv.francescovago@gmail.com>
2026-08-16 12:39:06 +0000

    refactor `input` parameter name to avoid shadowing builtin

--- claasp/ciphers/permutations/subterranean_permutation.py
+++ claasp/ciphers/permutations/subterranean_permutation.py
@@ -62,7 +62,7 @@ class SubterraneanPermutation(Cipher):
                 self.state_bit_size, self.key_bit_size] if version == Version.V1 else [
                 self.state_bit_size], cipher_output_bit_size=self.state_bit_size, )
 
-        input = ComponentState([INPUT_PLAINTEXT], [list(range(self.state_bit_size))])
+        state = ComponentState([INPUT_PLAINTEXT], [list(range(self.state_bit_size))])
         key = ComponentState([INPUT_KEY], [list(range(self.key_bit_size))])
 
         rounds = number_of_rounds
@@ -70,86 +70,86 @@ class SubterraneanPermutation(Cipher):
         for r in range(rounds):
             self.add_round()
 
-            input = self._step_1(input) if version == Version.V1 else self._chi(input)
-            input = self._step_2(input)
-            input = self._step_3(input)
+            state = self._step_1(state) if version == Version.V1 else self._chi(state)
+            state = self._step_2(state)
+            state = self._step_3(state)
             if version == Version.V1:
-                input = self._step_4(input, key)
-            input = self._step_5(input)
+                state = self._step_4(state, key)
+            state = self._step_5(state)
 
-            ids, bits = get_inputs_parameter([input])
+            ids, bits = get_inputs_parameter([state])
             if r == rounds - 1:
                 self.add_cipher_output_component(ids, bits, self.state_bit_size)
             else:
                 self.add_round_output_component(ids, bits, self.state_bit_size)
 
-    def _rotate(self, input: ComponentState, size: int, parameter: int) -> ComponentState:
-        ids, bits = get_inputs_parameter([input])
+    def _rotate(self, state: ComponentState, size: int, parameter: int) -> ComponentState:
+        ids, bits = get_inputs_parameter([state])
         self.add_rotate_component(ids, bits, size, parameter)
         return ComponentState([self.get_current_component_id()], [list(range(size))])
 
-    def _step_1(self, input: ComponentState) -> ComponentState:
+    def _step_1(self, state: ComponentState) -> ComponentState:
 
-        a2 = self._rotate(input, self.state_bit_size, -2)
+        a2 = self._rotate(state, self.state_bit_size, -2)
 
         ids, bits = get_inputs_parameter([a2])
         neg = self.add_not_component(ids, bits, self.state_bit_size)
 
         a2 = ComponentState([neg.id], [list(range(self.state_bit_size))])
 
-        a1 = self._rotate(input, self.state_bit_size, -1)
+        a1 = self._rotate(state, self.state_bit_size, -1)
 
         ids, bits = get_inputs_parameter([a1, a2])
         a = self.add_or_component(ids, bits, self.state_bit_size)
 
-        ids, bits = get_inputs_parameter([input])
+        ids, bits = get_inputs_parameter([state])
         self.add_xor_component(ids + [a.id], bits + [list(range(self.state_bit_size))], self.state_bit_size)
 
         return ComponentState([self.get_current_component_id()], [list(range(self.state_bit_size))])
 
-    def _step_2(self, input: ComponentState) -> ComponentState:
+    def _step_2(self, state: ComponentState) -> ComponentState:
         # negation of the first bit
-        neg = self.add_not_component([input.id[0]], [[input.input_bit_positions[0][0]]], 1)
+        neg = self.add_not_component([state.id[0]], [[state.input_bit_positions[0][0]]], 1)
 
-        return ComponentState([neg.id, *input.id], [[0], input.input_bit_positions[0][1:]])
+        return ComponentState([neg.id, *state.id], [[0], state.input_bit_positions[0][1:]])
 
-    def _step_3(self, input: ComponentState) -> ComponentState:
-        a3 = self._rotate(input, self.state_bit_size, -3)
-        a8 = self._rotate(input, self.state_bit_size, -8)
+    def _step_3(self, state: ComponentState) -> ComponentState:
+        a3 = self._rotate(state, self.state_bit_size, -3)
+        a8 = self._rotate(state, self.state_bit_size, -8)
 
-        ids, bits = get_inputs_parameter([input, a3, a8])
+        ids, bits = get_inputs_parameter([state, a3, a8])
         self.add_xor_component(ids, bits, self.state_bit_size)
 
         return ComponentState([self.get_current_component_id()], [list(range(self.state_bit_size))])
 
-    def _step_4(self, input: ComponentState, key: ComponentState) -> ComponentState:
-        xor = self.add_xor_component([input.id[0], key.id[0]], [input.input_bit_positions[0]
+    def _step_4(self, state: ComponentState, key: ComponentState) -> ComponentState:
+        xor = self.add_xor_component([state.id[0], key.id[0]], [state.input_bit_positions[0]
                                      [1:]] + key.input_bit_positions, self.state_bit_size - 1)
         return ComponentState(
-            [input.id[0], xor.id],
-            [[input.input_bit_positions[0][0]], list(range(self.key_bit_size))]
+            [state.id[0], xor.id],
+            [[state.input_bit_positions[0][0]], list(range(self.key_bit_size))]
         )
 
-    def _step_5(self, input: ComponentState) -> ComponentState:
+    def _step_5(self, state: ComponentState) -> ComponentState:
         permutation = [(pow(12, -1, self.state_bit_size) * i) % self.state_bit_size for i in range(self.state_bit_size)]
-        ids, bits = get_inputs_parameter([input])
+        ids, bits = get_inputs_parameter([state])
         self.add_permutation_component(ids, bits, self.state_bit_size, permutation)
         return ComponentState([self.get_current_component_id()], [list(range(self.state_bit_size))])
 
-    def _chi(self, input: ComponentState) -> ComponentState:
-        s1 = self._rotate(input, self.state_bit_size, - 1)
+    def _chi(self, state: ComponentState) -> ComponentState:
+        s1 = self._rotate(state, self.state_bit_size, - 1)
 
         ids, bits = get_inputs_parameter([s1])
         self.add_not_component(ids, bits, self.state_bit_size)
 
         not_s1 = ComponentState([self.get_current_component_id()], [list(range(self.state_bit_size))])
 
-        s2 = self._rotate(input, self.state_bit_size, -2)
+        s2 = self._rotate(state, self.state_bit_size, -2)
 
         ids, bits = get_inputs_parameter([not_s1, s2])
 
         mul = self.add_and_component(ids, bits, self.state_bit_size)
 
-        self.add_xor_component(input.id + [mul.id], input.input_bit_positions +
+        self.add_xor_component(state.id + [mul.id], state.input_bit_positions +
                                [list(range(self.state_bit_size))], self.state_bit_size)
         return ComponentState([self.get_current_component_id()], [list(range(self.state_bit_size))])
