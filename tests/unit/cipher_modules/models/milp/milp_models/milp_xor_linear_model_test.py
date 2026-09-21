98dc482ff66074dcb8870593f71b14d5dc0aff6b
paulhuynh <paul.huynh@tii.ae>
2026-09-21 23:15:32 +0400

    rename tests

--- tests/unit/cipher_modules/models/milp/milp_models/milp_xor_linear_model_test.py
+++ tests/unit/cipher_modules/models/milp/milp_models/milp_xor_linear_model_test.py
@@ -118,7 +118,7 @@ def test_find_one_xor_linear_trail_with_fixed_weight():
     # assert trail["total_weight"] == 10.0
 
 
-def test_find_one_xor_linear_trail_with_upper_bound():
+def test_find_one_xor_linear_trail_with_weight_at_most():
     speck = SpeckBlockCipher(block_bit_size=32, key_bit_size=64, number_of_rounds=2)
     trail = MilpXorLinearModel(speck).find_one_xor_linear_trail(upper_bound=6)
     assert 0.0 <= trail["total_weight"] <= 6.0
